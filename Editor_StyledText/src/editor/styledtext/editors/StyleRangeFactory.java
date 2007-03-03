879c32c9c89d4442dea87eb907f273634313a8ca
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-03-03 09:05:52 +0000

    Note wird stylisch dargestellt

---
+++ Editor_StyledText/src/editor/styledtext/editors/StyleRangeFactory.java
@@ -0,0 +1,28 @@
+package editor.styledtext.editors;
+
+import net.disy.commons.core.text.font.FontStyle;
+import net.sf.anathema.lib.textualdescription.ITextFormat;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
+
+public class StyleRangeFactory {
+
+  public StyleRange createStyleRange(final int startIndex, int length, ITextFormat format) {
+    FontStyle fontStyleEnum = format.getFontStyle();
+    StyleRange styleRange = new StyleRange(startIndex, length, null, null, convertToSwt(fontStyleEnum));
+    styleRange.underline = format.isUnderline();
+    return styleRange;
+  }
+
+  private static int convertToSwt(FontStyle fontStyle) {
+    int swtStyle = SWT.NORMAL;
+    if (fontStyle.isBold()) {
+      swtStyle |= SWT.BOLD;
+    }
+    if (fontStyle.isItalic()) {
+      swtStyle |= SWT.ITALIC;
+    }
+    return swtStyle;
+  }
+}
--- Editor_StyledText/src/editor/styledtext/editors/StyledTextEditor.java
+++ Editor_StyledText/src/editor/styledtext/editors/StyledTextEditor.java
@@ -1,13 +1,19 @@
 package editor.styledtext.editors;
 
+import net.disy.commons.core.util.ArrayUtilities;
+import net.disy.commons.core.util.ITransformer;
 import net.sf.anathema.basics.jface.IStorageEditorInput;
 import net.sf.anathema.framework.item.data.BasicItemData;
 import net.sf.anathema.framework.item.data.BasicsPersister;
+import net.sf.anathema.lib.textualdescription.ITextFormat;
+import net.sf.anathema.lib.textualdescription.ITextPart;
 import net.sf.anathema.lib.xml.DocumentUtilities;
 
 import org.dom4j.Document;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -73,9 +79,29 @@ public class StyledTextEditor extends EditorPart {
     Label contentLabel = new Label(parent, SWT.LEFT);
     contentLabel.setText("Content:"); //$NON-NLS-1$
     contentLabel.setLayoutData(new GridData(GridData.BEGINNING, GridData.BEGINNING, false, false));
-    Text contentText = new Text(parent, SWT.MULTI | SWT.V_SCROLL | SWT.WRAP | SWT.BORDER);
-    contentText.setLayoutData(new GridData(GridData.FILL_BOTH));
-    contentText.setText(itemData.getDescription().getContent().getText());
+    StyledText contentComposite = new StyledText(parent, SWT.MULTI | SWT.V_SCROLL | SWT.WRAP | SWT.BORDER);
+    contentComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+    StyleRange[] styleRanges = createStyleRanges();
+    contentComposite.setText(itemData.getDescription().getContent().getText());
+    contentComposite.setStyleRanges(styleRanges);
+  }
+
+  private StyleRange[] createStyleRanges() {
+    final StyleRangeFactory styleRangeFactory = new StyleRangeFactory();
+    final int[] startIndex = new int[] { 0 };
+    StyleRange[] styleRanges = ArrayUtilities.transform(
+        itemData.getDescription().getContent().getTextParts(),
+        StyleRange.class,
+        new ITransformer<ITextPart, StyleRange>() {
+          public StyleRange transform(ITextPart textPart) {
+            int length = textPart.getText().length();
+            ITextFormat format = textPart.getFormat();
+            StyleRange styleRange = styleRangeFactory.createStyleRange(startIndex[0], length, format);
+            startIndex[0] += length;
+            return styleRange;
+          }
+        });
+    return styleRanges;
   }
 
   @Override
