c672e1abbd7c08cca273b7292ee4836d21d017ed
Urs Reupke <ursreupke@gmail.com>
2007-05-01 10:48:41 +0000

    ExtensionPoint für itemtypes

--- Basics_Repository/src/net/sf/anathema/basics/repository/RepositoryPlugin.java
+++ Basics_Repository/src/net/sf/anathema/basics/repository/RepositoryPlugin.java
@@ -1,10 +1,8 @@
 package net.sf.anathema.basics.repository;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.Platform;
@@ -13,31 +11,28 @@ import org.osgi.framework.BundleContext;
 
 public class RepositoryPlugin extends AbstractUIPlugin {
 
+  private static final String ATTRIB_FOLDER = "folder"; //$NON-NLS-1$
+  private static final String REPOSITORY_ITEMTYPES = "net.sf.anathema.repository.itemtypes"; //$NON-NLS-1$
+
   @Override
   public void start(BundleContext context) throws Exception {
     super.start(context);
-    IExtension[] itemTypes = Platform.getExtensionRegistry()
-        .getExtensionPoint("net.sf.anathema.repository.itemtypes")
-        .getExtensions();
-    for (IExtension extension : itemTypes) {
+    String extensionName = REPOSITORY_ITEMTYPES;
+    for (IExtension extension : Platform.getExtensionRegistry().getExtensionPoint(extensionName).getExtensions()) {
       for (IConfigurationElement configurationElement : extension.getConfigurationElements()) {
-        String folderName = configurationElement.getAttribute("folder");
+        String folderName = configurationElement.getAttribute(ATTRIB_FOLDER);
         initProject(folderName);
       }
     }
   }
 
-  private void initProject(String projectName) {
+  private void initProject(String projectName) throws Exception {
     IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
     IProject project = root.getProject(projectName);
-    if (!project.exists()) {
-      try {
-        project.create(null);
-        project.open(null);
-      }
-      catch (CoreException e) {
-        e.printStackTrace();
-      }
+    if (project.exists()) {
+      return;
     }
+    project.create(null);
+    project.open(null);
   }
 }
