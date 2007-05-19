9dae83bc8b5b9f90b2b31243b90449e24fbcf868
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-05-19 12:24:00 +0000

    Tut nix Test

---
+++ Basics_Item/test/net/sf/anathema/framework/editor/NewItemEditorInputTest.java
@@ -0,0 +1,29 @@
+package net.sf.anathema.framework.editor;
+
+import net.sf.anathema.framework.persistence.BasicDataItemPersister;
+import net.sf.anathema.lib.exception.PersistenceException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NewItemEditorInputTest {
+
+  private NewItemEditorInput editorInput;
+
+  @Before
+  public void createFilledEditorInput() throws PersistenceException, CoreException {
+    this.editorInput = new NewItemEditorInput();
+    this.editorInput.loadItem(new BasicDataItemPersister());
+  }
+  
+  @Test
+  public void createsNewFileOnSave() throws Exception {
+    BasicDataItemPersister persister = new BasicDataItemPersister();
+    editorInput.save(persister);
+  }
+  
+  public void deleteFiles() {
+    
+  }
+}
