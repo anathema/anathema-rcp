2b728707fb92a9cf2df97aa6af2c574e655c2b0c
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-05-20 14:02:32 +0000

    SeriesViewElements haben jetzt ein Icon

--- Campaign_Plot/src/net/sf/anathema/campaign/plot/repository/SeriesViewElement.java
+++ Campaign_Plot/src/net/sf/anathema/campaign/plot/repository/SeriesViewElement.java
@@ -1,6 +1,9 @@
 package net.sf.anathema.campaign.plot.repository;
 
+import net.disy.commons.core.util.StringUtilities;
+import net.sf.anathema.basics.repository.treecontent.itemtype.IPrintNameProvider;
 import net.sf.anathema.basics.repository.treecontent.itemtype.IViewElement;
+import net.sf.anathema.basics.repository.treecontent.itemtype.RegExPrintNameProvider;
 
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.swt.graphics.Image;
@@ -12,6 +15,7 @@ public class SeriesViewElement implements IViewElement {
   private final IFolder resource;
   private final IViewElement parent;
   private final String untitledName;
+  private final IPrintNameProvider printNameProvider = new RegExPrintNameProvider();
 
   public SeriesViewElement(IFolder resource, IViewElement parent, String untitledName) {
     this.resource = resource;
@@ -26,12 +30,16 @@ public class SeriesViewElement implements IViewElement {
 
   @Override
   public String getDisplayName() {
-    return resource.getName();
+    String printName = printNameProvider.getPrintName(resource.getFile("main.srs")); //$NON-NLS-1$
+    if (StringUtilities.isNullOrTrimEmpty(printName)) {
+      return untitledName;
+    }
+    return printName;
   }
 
   @Override
   public Image getImage() {
-    return null;
+    return parent.getImage();
   }
 
   @Override
