804802201247bd1ce894f1e76370ac5f380cde01
Urs Reupke <ursreupke@gmail.com>
2007-05-28 08:24:13 +0000

    REmoved unused method

--- Campaign_Note/src/net/sf/anathema/campaign/note/NotesRepositoryUtilities.java
+++ Campaign_Note/src/net/sf/anathema/campaign/note/NotesRepositoryUtilities.java
@@ -1,8 +1,5 @@
 package net.sf.anathema.campaign.note;
 
-import org.eclipse.core.resources.IProject;
-
-import net.sf.anathema.basics.repository.access.RepositoryUtilities;
 import net.sf.anathema.basics.repository.itemtype.IItemType;
 import net.sf.anathema.basics.repository.itemtype.ItemTypeProvider;
 
@@ -11,10 +8,4 @@ public class NotesRepositoryUtilities {
   public static IItemType getNotesItemType() {
     return new ItemTypeProvider().getById("net.sf.anathema.itemtype.Note"); //$NON-NLS-1$
   }
-
-  public static IProject getNotesProject() {
-    IItemType notesItemType = getNotesItemType();
-    return RepositoryUtilities.getProject(notesItemType);
-  }
-
 }
