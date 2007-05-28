23ec5492dff99a2aba600ae9869bf42bec5b3545
Urs Reupke <ursreupke@gmail.com>
2007-05-28 13:16:34 +0000

    Folder-Based CharacterViewElement

---
+++ Character_Core/src/net/sf/anathema/character/core/repository/CharacterViewElement.java
@@ -0,0 +1,64 @@
+package net.sf.anathema.character.core.repository;
+
+import net.sf.anathema.basics.repository.treecontent.itemtype.IViewElement;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+
+public class CharacterViewElement implements IViewElement {
+  
+  private final IFolder characterFolder;
+  private final IViewElement parent;
+
+  public CharacterViewElement(IViewElement parent, IFolder characterFolder) {
+    this.parent = parent;
+    this.characterFolder = characterFolder;
+  }
+
+  @Override
+  public IViewElement[] getChildren() {
+    return new IViewElement[0];
+  }
+
+  @Override
+  public String getDisplayName() {
+    return characterFolder.getName();
+  }
+
+  @Override
+  public IViewElement getParent() {
+    return parent;
+  }
+
+  @Override
+  public boolean hasChildren() {
+    return false;
+  }
+
+
+  @Override
+  public boolean equals(Object object) {
+    if (!(object instanceof CharacterViewElement)) {
+      return false;
+    }
+    CharacterViewElement other = (CharacterViewElement) object;
+    return characterFolder.equals(other.characterFolder);
+  }
+
+  @Override
+  public int hashCode() {
+    return characterFolder.hashCode();
+  }
+
+  @Override
+  public Image getImage() {
+    return parent.getImage();
+  }
+
+  @Override
+  public void openEditor(IWorkbenchPage page) throws PartInitException {
+    // nothing to do
+  }
+}
--- Character_Core/src/net/sf/anathema/character/core/repository/CharacterViewElementFactory.java
+++ Character_Core/src/net/sf/anathema/character/core/repository/CharacterViewElementFactory.java
@@ -3,30 +3,19 @@ package net.sf.anathema.character.core.repository;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.sf.anathema.basics.repository.itemtype.IItemType;
-import net.sf.anathema.basics.repository.treecontent.itemtype.IItemTypeViewElementFactory;
+import net.sf.anathema.basics.repository.treecontent.itemtype.AbstractFolderBasedViewElementFactory;
 import net.sf.anathema.basics.repository.treecontent.itemtype.IViewElement;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.resources.IFolder;
 
-public class CharacterViewElementFactory implements IItemTypeViewElementFactory {
+public class CharacterViewElementFactory extends AbstractFolderBasedViewElementFactory {
 
   @Override
   public List<IViewElement> createViewElements(IViewElement parent) {
-    return new ArrayList<IViewElement>();
-  }
-
-  @Override
-  public void setItemType(IItemType itemType) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void setInitializationData(IConfigurationElement config, String propertyName, Object data)
-      throws CoreException {
-    // TODO Auto-generated method stub
-
+    List<IViewElement> elements = new ArrayList<IViewElement>();
+    for (IFolder folder : getMembers()) {
+      elements.add(new CharacterViewElement(parent, folder));
+    }
+    return elements;
   }
 }
