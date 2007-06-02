cc9f3c8c8fe49f0813a27ccc64e663977c9de2c4
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-06-02 15:07:56 +0000

    In der richtigen XML-Tiefe suchen

--- Character_Core/src/net/sf/anathema/character/description/CharacterDescriptionPersister.java
+++ Character_Core/src/net/sf/anathema/character/description/CharacterDescriptionPersister.java
@@ -6,14 +6,16 @@ import java.io.OutputStream;
 import net.sf.anathema.basics.item.persistence.ISingleFileItemPersister;
 import net.sf.anathema.basics.item.text.TextPersister;
 import net.sf.anathema.lib.exception.PersistenceException;
+import net.sf.anathema.lib.xml.DocumentUtilities;
 
 import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 
 public class CharacterDescriptionPersister implements ISingleFileItemPersister<ICharacterDescription> {
 
   private static final String TAG_CHARACTERIZATION = "Characterization"; //$NON-NLS-1$
-  private static final String TAG_CHARACTER_NAME = "CharacterName"; //$NON-NLS-1$
+  private static final String TAG_CHARACTER_NAME = "Name"; //$NON-NLS-1$
   private static final String TAG_NOTES = "Notes"; //$NON-NLS-1$
   private static final String TAG_PERIPHRASE = "Periphrasis"; //$NON-NLS-1$
   private static final String TAG_PHYSICAL_DESCRIPTION = "PhysicalDescription"; //$NON-NLS-1$
@@ -24,12 +26,11 @@ public class CharacterDescriptionPersister implements ISingleFileItemPersister<I
   private final TextPersister textPersister = new TextPersister();
 
   public ICharacterDescription load(Document document) {
-    Element parent = document.getRootElement();
     ICharacterDescription description = createNew();
-    Element descriptionElement = parent.element(TAG_DESCRIPTION);
-    if (descriptionElement == null) {
-      return createNew();
+    if (document == null) {
+      return description;
     }
+    Element descriptionElement = document.getRootElement();
     textPersister.restoreTextualDescription(descriptionElement, TAG_CHARACTER_NAME, description.getName());
     textPersister.restoreTextualDescription(descriptionElement, TAG_PLAYER, description.getPlayer());
     textPersister.restoreTextualDescription(descriptionElement, TAG_CHARACTERIZATION, description.getCharacterization());
@@ -43,8 +44,7 @@ public class CharacterDescriptionPersister implements ISingleFileItemPersister<I
     return description;
   }
 
-  public void save(Element parent, ICharacterDescription description) {
-    Element descriptionElement = parent.addElement(TAG_DESCRIPTION);
+  public void save(Element descriptionElement, ICharacterDescription description) {
     textPersister.saveTextualDescription(descriptionElement, TAG_CHARACTER_NAME, description.getName());
     textPersister.saveTextualDescription(descriptionElement, TAG_PLAYER, description.getPlayer());
     textPersister.saveTextualDescription(descriptionElement, TAG_CHARACTERIZATION, description.getCharacterization());
@@ -59,8 +59,9 @@ public class CharacterDescriptionPersister implements ISingleFileItemPersister<I
 
   @Override
   public void save(OutputStream stream, ICharacterDescription item) throws IOException, PersistenceException {
-    // TODO Auto-generated method stub
-
+    Document document = DocumentHelper.createDocument(DocumentHelper.createElement(TAG_DESCRIPTION));
+    save(document.getRootElement(), item);
+    DocumentUtilities.save(document, stream);
   }
 
   @Override
