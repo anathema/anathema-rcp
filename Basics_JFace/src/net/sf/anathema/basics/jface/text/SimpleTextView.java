8e0153367f6fe3d2e1ac2b5b680cd7b00a64cc9b
Urs Reupke <ursreupke@gmail.com>
2007-06-07 08:30:24 +0000

    Scrollbar, multilines for multitext

--- Basics_JFace/src/net/sf/anathema/basics/jface/text/SimpleTextView.java
+++ Basics_JFace/src/net/sf/anathema/basics/jface/text/SimpleTextView.java
@@ -1,11 +1,11 @@
 package net.sf.anathema.basics.jface.text;
 
+import static org.eclipse.swt.SWT.*;
 import net.disy.commons.core.util.ObjectUtilities;
 import net.sf.anathema.lib.control.objectvalue.IObjectValueChangedListener;
 import net.sf.anathema.lib.control.objectvalue.ObjectValueControl;
 import net.sf.anathema.lib.textualdescription.ITextView;
 
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
@@ -16,23 +16,26 @@ public class SimpleTextView implements ITextView {
 
   private final Text textWidget;
   private final ObjectValueControl<String> control = new ObjectValueControl<String>();
-  
+
   public static SimpleTextView createSingleLineView(Composite parent) {
-    return new SimpleTextView(parent, SWT.SINGLE);
+    SimpleTextView view = new SimpleTextView(parent, SINGLE);
+    view.textWidget.setLayoutData(new GridData(FILL, DEFAULT, true, false));
+    return view;
   }
 
   public static SimpleTextView createMultiLineView(Composite parent) {
-    return new SimpleTextView(parent, SWT.MULTI);
+    SimpleTextView view = new SimpleTextView(parent, MULTI | WRAP | V_SCROLL);
+    view.textWidget.setLayoutData(new GridData(FILL, FILL, true, true));
+    return view;
   }
 
   private SimpleTextView(Composite parent, int lineFlag) {
-    this.textWidget = new Text(parent, lineFlag | SWT.BORDER);
+    this.textWidget = new Text(parent, lineFlag | BORDER);
     this.textWidget.addModifyListener(new ModifyListener() {
       public void modifyText(ModifyEvent e) {
         control.fireValueChangedEvent(textWidget.getText());
       }
     });
-    textWidget.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
   }
 
   @Override
@@ -52,7 +55,7 @@ public class SimpleTextView implements ITextView {
     }
     textWidget.setText(text == null ? "" : text); //$NON-NLS-1$
   }
-  
+
   @Override
   public void setFocus() {
     textWidget.setFocus();
