e345cdb61f7ffd92a1f5734f0873c2908069f2b2
Urs Reupke <ursreupke@gmail.com>
2007-06-10 17:06:22 +0000

    Reinstated redraw ... :(

--- Character_Core/demo/net/sf/anathema/character/trait/LabelPaintListener.java
+++ Character_Core/demo/net/sf/anathema/character/trait/LabelPaintListener.java
@@ -39,5 +39,6 @@ public final class LabelPaintListener implements PaintListener {
     int rectangleWidth = Math.max(0, width - labelPosition);
     rectangleToDraw.width = Math.min(rectangleWidth, label.getSize().x);
     rectangleToDraw.height = label.getSize().y;
+    label.redraw();
   }
 }
