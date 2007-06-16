dd716afb069878badfcff544310e529cbb10d760
Urs Reupke <ursreupke@gmail.com>
2007-06-16 10:59:36 +0000

    Calculate Preferred Size

--- Character_Core/demo/net/sf/anathema/character/trait/CanvasIntValueDisplay.java
+++ Character_Core/demo/net/sf/anathema/character/trait/CanvasIntValueDisplay.java
@@ -9,10 +9,9 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
-import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
@@ -76,9 +75,14 @@ public class CanvasIntValueDisplay implements ISWTIntValueDisplay {
   }
 
   public Composite createComposite(Composite parent) {
-    this.canvas = new Canvas(parent, SWT.DOUBLE_BUFFERED);
-    Point computeSize = canvas.computeSize(SWT.DEFAULT, passiveImage.getImageData().height);
-    canvas.setSize(computeSize);
+    this.canvas = new Canvas(parent, SWT.DOUBLE_BUFFERED) {
+      @Override
+      public Rectangle computeTrim(int x, int y, int width, int height) {
+        int preferredHeight = passiveImage.getImageData().height + 2;
+        int preferredWidth = slotWidth * maxValue;
+        return new Rectangle(x, y, preferredWidth, preferredHeight);
+      }
+    };
     mouseListener.addTo(canvas);
     canvas.addPaintListener(new PaintListener() {
       @Override
