72c84965a0e61d8644df3def1b905edccbbb15e8
Urs Reupke <ursreupke@gmail.com>
2007-06-17 06:15:03 +0000

    Content conversion for series, but not yet fully done.

--- Campaign_Plot/src/net/sf/anathema/campaign/plot/repository/conversion/LegacyPlotConverter.java
+++ Campaign_Plot/src/net/sf/anathema/campaign/plot/repository/conversion/LegacyPlotConverter.java
@@ -12,11 +12,21 @@ import org.dom4j.Document;
 import org.dom4j.io.DocumentResult;
 import org.dom4j.io.DocumentSource;
 
-public class HierarchyCreation {
+public class LegacyPlotConverter {
 
-  public Document run(Document sourceDocument) throws TransformerException, FileNotFoundException {
+  public static Document createContent(Document sourceDocument) throws FileNotFoundException, TransformerException {
+    return run(sourceDocument, "xsl/ContentCreation.xsl"); //$NON-NLS-1$
+  }
+  
+  public static Document createHierarchy(Document sourceDocument) throws FileNotFoundException, TransformerException {
+    return run(sourceDocument, "xsl/HierarchyCreation.xsl"); //$NON-NLS-1$
+  }
+
+  private static Document run(Document sourceDocument, String stylesheet)
+      throws TransformerException,
+      FileNotFoundException {
     TransformerFactory factory = TransformerFactory.newInstance();
-    Transformer transformer = factory.newTransformer(new StreamSource(new FileInputStream("xsl/HierarchyCreation.xsl"))); //$NON-NLS-1$
+    Transformer transformer = factory.newTransformer(new StreamSource(new FileInputStream(stylesheet)));
     DocumentSource source = new DocumentSource(sourceDocument);
     DocumentResult result = new DocumentResult();
     transformer.transform(source, result);
--- Campaign_Plot/test/net/sf/anathema/campaign/plot/repository/HierarchyConversionTest.java
+++ Campaign_Plot/test/net/sf/anathema/campaign/plot/repository/HierarchyConversionTest.java
@@ -2,7 +2,7 @@ package net.sf.anathema.campaign.plot.repository;
 
 import java.io.InputStream;
 
-import net.sf.anathema.campaign.plot.repository.conversion.HierarchyCreation;
+import net.sf.anathema.campaign.plot.repository.conversion.LegacyPlotConverter;
 import net.sf.anathema.lib.exception.PersistenceException;
 import net.sf.anathema.lib.xml.DocumentUtilities;
 
@@ -17,10 +17,18 @@ public class HierarchyConversionTest {
   public void createsHierarchy() throws Exception {
     Document document = readDocument("oldmain.srs"); //$NON-NLS-1$
     Document expecteddocument = readDocument("newhierarchy.xml"); //$NON-NLS-1$
-    Document resultdocument = new HierarchyCreation().run(document);
+    Document resultdocument = LegacyPlotConverter.createHierarchy(document);
     Assert.assertEquals(expecteddocument.asXML(), resultdocument.asXML());
   }
 
+  @Test
+  public void cleansUpContent() throws Exception {
+    Document document = readDocument("oldmain.srs"); //$NON-NLS-1$
+    Document expecteddocument = readDocument("newmain.srs"); //$NON-NLS-1$
+    Document resultdocument = LegacyPlotConverter.createContent(document);
+    Assert.assertEquals(expecteddocument.asXML(), resultdocument.asXML());   
+  }
+  
   private Document readDocument(String string) throws PersistenceException {
     return DocumentUtilities.read(getStream(string));
   }
