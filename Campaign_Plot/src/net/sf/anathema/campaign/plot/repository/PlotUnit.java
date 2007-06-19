9e6db3ff24ab2843ef3096bdb2ffa9ae11ba21c7
Urs Reupke <ursreupke@gmail.com>
2007-06-19 15:25:45 +0000

    Comment about necessity of PlotUnits

--- Campaign_Plot/src/net/sf/anathema/campaign/plot/repository/PlotUnit.java
+++ Campaign_Plot/src/net/sf/anathema/campaign/plot/repository/PlotUnit.java
@@ -1,5 +1,7 @@
 package net.sf.anathema.campaign.plot.repository;
 
+//TODO: Maybe we could eliminate PlotUnits?
+//At this point they are only necessary for I18n, which could be done via ext. points or a short XML file. 
 public enum PlotUnit {
 
   Scene(null), Episode(PlotUnit.Scene), Story(PlotUnit.Episode), Plot(PlotUnit.Story);
