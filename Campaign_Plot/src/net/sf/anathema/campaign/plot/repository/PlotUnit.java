7fff4b9afd1c3252783a84e958c8f5383e450b0e
Urs Reupke <ursreupke@gmail.com>
2007-06-19 15:35:46 +0000

    Cleanup

--- Campaign_Plot/src/net/sf/anathema/campaign/plot/repository/PlotUnit.java
+++ Campaign_Plot/src/net/sf/anathema/campaign/plot/repository/PlotUnit.java
@@ -1,19 +1,16 @@
 package net.sf.anathema.campaign.plot.repository;
 
-//TODO: Maybe we could eliminate PlotUnits?
-//At this point they are only necessary for I18n, which could be done via ext. points or a short XML file. 
+// TODO: Maybe we could eliminate PlotUnits?
+// At this point they are only necessary for I18n, which could be done via ext. points or a short XML file.
 public enum PlotUnit {
 
-  Scene(null), Episode(PlotUnit.Scene), Story(PlotUnit.Episode), Plot(PlotUnit.Story);
-
-  private final PlotUnit successor;
-
-  private PlotUnit(PlotUnit successor) {
-    this.successor = successor;
-  }
+  Scene, Episode, Story, Plot;
 
   public PlotUnit getSuccessor() {
-    return successor == null ? Scene : successor;
+    if (ordinal() == 0) {
+      return Scene;
+    }
+    return values()[ordinal() - 1];
   }
 
   public String getPersistenceString() {
