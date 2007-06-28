6842dfb1bd3811bb62f6b889a8ceceaa7f891ff1
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-06-28 12:42:07 +0000

    IModel ist ein IItem

---
+++ Character_Core/src/net/sf/anathema/character/basics/DummyCharacterBasics.java
@@ -0,0 +1,21 @@
+package net.sf.anathema.character.basics;
+
+import net.sf.anathema.character.core.model.AbstractModel;
+
+public class DummyCharacterBasics extends AbstractModel implements ICharacterBasics {
+
+  private final boolean experienced;
+
+  public DummyCharacterBasics() {
+    this(false);
+  }
+  
+  public DummyCharacterBasics(boolean experienced) {
+    this.experienced = experienced;
+  }
+  
+  @Override
+  public boolean isExperienced() {
+    return experienced;
+  }
+}
---
+++ Character_Core/src/net/sf/anathema/character/core/model/AbstractModel.java
@@ -0,0 +1,16 @@
+package net.sf.anathema.character.core.model;
+
+import net.sf.anathema.lib.control.ChangeManagement;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+
+public class AbstractModel extends ChangeManagement implements IModel{
+
+  @Override
+  public final void setInitializationData(IConfigurationElement config, String propertyName, Object data)
+      throws CoreException {
+    // nothing to do
+    System.err.println(config);
+  }
+}
