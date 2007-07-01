225f9015f27107c325d0edb9feb54f5eab6729f2
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-07-01 13:17:54 +0000

    RulesTraitToggleExperienceTest am Start

---
+++ Character_Core/test/net/sf/anathema/character/trait/rules/RulesTraitToggleExperiencedTest.java
@@ -0,0 +1,39 @@
+package net.sf.anathema.character.trait.rules;
+
+import static org.junit.Assert.*;
+import net.sf.anathema.character.experience.IExperience;
+import net.sf.anathema.character.trait.BasicTrait;
+import net.sf.anathema.character.trait.DummyTraitTemplate;
+import net.sf.anathema.character.trait.experience.DummyExperience;
+import net.sf.anathema.lib.util.Identificate;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RulesTraitToggleExperiencedTest {
+
+  private static final int CREATION_VALUE = 3;
+  private static final int EXPERIENCED_VALUE = 5;
+  private IExperience experience;
+  private RuleTrait ruleTrait;
+
+  @Before
+  public void createRules() throws Exception {
+    this.experience = new DummyExperience(false);
+    BasicTrait basicTrait = new BasicTrait(new Identificate("Hasä")); //$NON-NLS-1$
+    this.ruleTrait = new RuleTrait(basicTrait, experience, new DummyTraitTemplate());
+    basicTrait.getCreationModel().setValue(CREATION_VALUE);
+    basicTrait.getExperiencedModel().setValue(EXPERIENCED_VALUE);
+  }
+  
+  @Test
+  public void foo() throws Exception {
+    assertEquals(CREATION_VALUE, ruleTrait.getValue());
+    experience.setExperienced(true);
+    assertEquals(EXPERIENCED_VALUE, ruleTrait.getValue());
+    experience.setExperienced(false);
+    assertEquals(CREATION_VALUE, ruleTrait.getValue());
+    experience.setExperienced(true);
+    assertEquals(EXPERIENCED_VALUE, ruleTrait.getValue());
+  }
+}
