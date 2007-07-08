2e37689189540e2f939e909339493af430afa5df
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-07-08 11:22:46 +0000

    Update geht ja doch.

--- Character_Core/src/net/sf/anathema/character/experiencepoints/ExperiencePointsView.java
+++ Character_Core/src/net/sf/anathema/character/experiencepoints/ExperiencePointsView.java
@@ -5,26 +5,38 @@ import net.sf.anathema.basics.eclipse.ui.PartListening;
 import net.sf.anathema.basics.eclipse.ui.TopPartListener;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
-public class ExperiencePointsView  extends DisposableViewPart {
-  
+public class ExperiencePointsView extends DisposableViewPart {
+
   private TopPartListener topPartListener = new TopPartListener(new Runnable() {
     @Override
     public void run() {
       updateToEditorChange();
     }
   });
+  private Composite component;
 
   @Override
   public void createPartControl(Composite parent) {
     addDisposable(new PartListening(topPartListener, getSite().getWorkbenchWindow().getPartService()));
-    new Composite(parent, SWT.NONE);
+    createComposite(parent);
+  }
+
+  private void createComposite(Composite parent) {
+    component = new Composite(parent, SWT.NONE);
+    component.setLayout(new GridLayout(1, false));
+    Label label = new Label(component, SWT.LEFT);
+    label.setText(String.valueOf(System.currentTimeMillis()));
+    component.pack();
   }
 
   protected void updateToEditorChange() {
-    // TODO Auto-generated method stub
-    
+    Composite parent = component.getParent();
+    component.dispose();
+    createComposite(parent);
   }
 
   @Override
