7abe2b910a38ea3a1e2f49a06761ae5b29df6abc
Urs Reupke <ursreupke@gmail.com>
2007-07-14 12:07:55 +0000

    Test für CharactereEditorInput am Start

---
+++ Character_Core/test/net/sf/anathema/character/points/DummyCharacterId.java
@@ -0,0 +1,21 @@
+package net.sf.anathema.character.points;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.anathema.basics.eclipse.resource.IContentHandle;
+import net.sf.anathema.character.core.model.ICharacterId;
+
+public class DummyCharacterId implements ICharacterId {
+
+  private final Map<String, IContentHandle> contentHandlesByName = new HashMap<String, IContentHandle>();
+
+  @Override
+  public IContentHandle getContents(String fileName) {
+    return contentHandlesByName.get(fileName);
+  }
+  
+  public void addContentHandle(String name, IContentHandle handle) {
+    contentHandlesByName.put(name, handle);
+  }
+}
--- Character_Core/test/net/sf/anathema/character/points/PointViewInputFactoryTest.java
+++ Character_Core/test/net/sf/anathema/character/points/PointViewInputFactoryTest.java
@@ -3,6 +3,7 @@ package net.sf.anathema.character.points;
 import static org.junit.Assert.*;
 import net.sf.anathema.basics.item.editor.IEditorInputProvider;
 import net.sf.anathema.character.core.model.IModelIdentifier;
+import net.sf.anathema.character.core.model.ModelIdentifier;
 
 import org.easymock.EasyMock;
 import org.eclipse.ui.IEditorInput;
@@ -12,7 +13,6 @@ import org.junit.Test;
 public class PointViewInputFactoryTest {
 
   private PointViewInputFactory viewInputFactory;
-
 
   private void assertEmptyViewElement(IPointViewInput newInput) {
     assertNotNull(newInput);
@@ -24,13 +24,13 @@ public class PointViewInputFactoryTest {
   public void createViewInputFactory() throws Exception {
     this.viewInputFactory = new PointViewInputFactory();
   }
-  
+
   @Test
   public void nullInputProviderReturnsEmptyViewInput() throws Exception {
     IPointViewInput newInput = viewInputFactory.createEditorInput(null, null);
     assertEmptyViewElement(newInput);
   }
-  
+
   @Test
   public void nonCharacterEditorInputReturnsEmptyViewInput() throws Exception {
     IEditorInput editorInput = EasyMock.createStrictMock(IEditorInput.class);
@@ -41,5 +41,27 @@ public class PointViewInputFactoryTest {
     IPointViewInput newInput = viewInputFactory.createEditorInput(inputProvider, null);
     assertEmptyViewElement(newInput);
     EasyMock.verify(inputProvider, editorInput);
+  }
+
+  @Test
+  public void createsEntriesForCharacterEditorInput() throws Exception {
+    DummyCharacterId characterId = new DummyCharacterId();
+    addTemplateHandle(characterId);
+    IEditorInput editorInput = EasyMock.createStrictMock(IEditorInput.class);
+    ModelIdentifier egalIdentifier = new ModelIdentifier(characterId, "Hasän.egal.id"); //$NON-NLS-1$
+    EasyMock.expect(editorInput.getAdapter(IModelIdentifier.class)).andReturn(egalIdentifier);
+    IEditorInputProvider inputProvider = EasyMock.createStrictMock(IEditorInputProvider.class);
+    EasyMock.expect(inputProvider.getEditorInput()).andReturn(editorInput);
+    EasyMock.replay(inputProvider, editorInput);
+    IPointViewInput newInput = viewInputFactory.createEditorInput(inputProvider, null);
+    assertNotNull(newInput);
+    assertNotNull(newInput.getCharacterId());
+    assertTrue(newInput.createEntries().length != 0);
+    EasyMock.verify(inputProvider, editorInput);
+  }
+
+  private void addTemplateHandle(DummyCharacterId characterId) {
+    characterId.addContentHandle(
+        "template.xml", new DummyContentHandle("<template reference=\"net.sf.anathema.core.StaticTemplate\" />")); //$NON-NLS-1$//$NON-NLS-2$
   }
 }
