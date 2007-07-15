29cffdedbded89d3e73195662e7e64f618083258
Urs Reupke <ursreupke@gmail.com>
2007-07-15 05:36:33 +0000

    Test against unchanged input when changing experience

--- Character_Core/test/net/sf/anathema/character/points/DummyCharacterId.java
+++ Character_Core/test/net/sf/anathema/character/points/DummyCharacterId.java
@@ -14,8 +14,13 @@ public class DummyCharacterId implements ICharacterId {
   public IContentHandle getContents(String fileName) {
     return contentHandlesByName.get(fileName);
   }
-  
+
   public void addContentHandle(String name, IContentHandle handle) {
     contentHandlesByName.put(name, handle);
+  }
+
+  @Override
+  public boolean equals(Object arg0) {
+    return arg0 instanceof DummyCharacterId;
   }
 }
--- Character_Core/test/net/sf/anathema/character/points/PointViewInputFactoryTest.java
+++ Character_Core/test/net/sf/anathema/character/points/PointViewInputFactoryTest.java
@@ -3,7 +3,9 @@ package net.sf.anathema.character.points;
 import static org.junit.Assert.*;
 import net.sf.anathema.basics.item.editor.IEditorInputProvider;
 import net.sf.anathema.character.core.model.IModelIdentifier;
+import net.sf.anathema.character.core.model.ModelCache;
 import net.sf.anathema.character.core.model.ModelIdentifier;
+import net.sf.anathema.character.experience.IExperience;
 
 import org.easymock.EasyMock;
 import org.eclipse.ui.IEditorInput;
@@ -75,8 +77,31 @@ public class PointViewInputFactoryTest {
     assertEquals(oldInput, newInput);
   }
 
+  @Test
+  public void createsNewInputForIdenticalCharacterEditorInputIfExperienceStateChanged() throws Exception {
+    DummyCharacterId characterId = new DummyCharacterId();
+    addTemplateHandle(characterId);
+    addExperienceHandle(characterId);
+    IEditorInput editorInput = EasyMock.createStrictMock(IEditorInput.class);
+    ModelIdentifier egalIdentifier = new ModelIdentifier(characterId, "Hasän.egal.id"); //$NON-NLS-1$
+    EasyMock.expect(editorInput.getAdapter(IModelIdentifier.class)).andReturn(egalIdentifier).anyTimes();
+    IEditorInputProvider inputProvider = EasyMock.createStrictMock(IEditorInputProvider.class);
+    EasyMock.expect(inputProvider.getEditorInput()).andReturn(editorInput).anyTimes();
+    EasyMock.replay(inputProvider, editorInput);
+    IPointViewInput oldInput = viewInputFactory.createEditorInput(inputProvider);
+    IExperience experience = (IExperience) ModelCache.getInstance().getModel(
+        new ModelIdentifier(characterId, IExperience.MODEL_ID));
+    experience.setExperienced(true);
+    IPointViewInput newInput = viewInputFactory.createEditorInput(inputProvider);
+    assertFalse(oldInput.equals(newInput));
+  }
+
   private void addTemplateHandle(DummyCharacterId characterId) {
     characterId.addContentHandle(
         "template.xml", new DummyContentHandle("<template reference=\"net.sf.anathema.core.StaticTemplate\" />")); //$NON-NLS-1$//$NON-NLS-2$
+  }
+
+  private void addExperienceHandle(DummyCharacterId characterId) {
+    characterId.addContentHandle("experience.model", new DummyContentHandle("<model experienced=\"false\"/>")); //$NON-NLS-1$
   }
 }
