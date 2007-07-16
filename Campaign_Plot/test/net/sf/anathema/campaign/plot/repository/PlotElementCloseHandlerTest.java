1ba6595883917471e9587b62854984a161d9da7b
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-07-16 14:17:59 +0000

    Noch mehr Duplikationen entfernt

--- Campaign_Plot/test/net/sf/anathema/campaign/plot/repository/PlotElementCloseHandlerTest.java
+++ Campaign_Plot/test/net/sf/anathema/campaign/plot/repository/PlotElementCloseHandlerTest.java
@@ -1,6 +1,10 @@
 package net.sf.anathema.campaign.plot.repository;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import net.sf.anathema.basics.jface.IFileEditorInput;
 
 import org.easymock.EasyMock;
@@ -19,14 +23,30 @@ public class PlotElementCloseHandlerTest {
   private IFile editorFile;
   private IFolder parentFolder;
   private IEditorReference reference;
+  private List<Object> additionalMocks = new ArrayList<Object>();
 
-  private void assertCloserIsClosed(boolean closed, Object... additionalMocks) throws PartInitException {
+  private void assertCloserIsClosed(boolean closed) throws PartInitException {
+    Object[] allAdditionalMocks = additionalMocks.toArray(new Object[additionalMocks.size()]);
     EasyMock.replay(reference, input, editorFile);
-    EasyMock.replay(additionalMocks);
+    EasyMock.replay(allAdditionalMocks);
     handler.closeIfRequired(reference);
     assertEquals(closed, closer.isClosed());
     EasyMock.verify(reference, input, editorFile);
-    EasyMock.verify(additionalMocks);
+    EasyMock.verify(allAdditionalMocks);
+  }
+
+  private IFile createMockFile(IFolder parent) {
+    IFile file = EasyMock.createMock(IFile.class);
+    EasyMock.expect(file.getParent()).andReturn(parent);
+    additionalMocks.add(file);
+    return file;
+  }
+
+  private DummyPlotElementViewElement addDummyViewElementTo(DummyPlotElementViewElement parent) {
+    DummyPlotElementViewElement child = new DummyPlotElementViewElement();
+    child.setParentFolder(parentFolder);
+    parent.addChild(child);
+    return child;
   }
 
   @Before
@@ -45,20 +65,12 @@ public class PlotElementCloseHandlerTest {
 
   @Test
   public void closesEditorHandlingElement() throws Exception {
-    EasyMock.expect(input.getAdapter(IFileEditorInput.class)).andReturn(input).anyTimes();
     IFolder parent = EasyMock.createMock(IFolder.class);
-    IFile file = createMockFile(parent);
-    EasyMock.expect(input.getFile()).andReturn(file);
-
-    element.setParentFolder(parent);
+    EasyMock.expect(input.getAdapter(IFileEditorInput.class)).andReturn(input).anyTimes();
+    EasyMock.expect(input.getFile()).andReturn(createMockFile(parent));
     EasyMock.expect(reference.getEditor(false)).andReturn(null);
-    assertCloserIsClosed(true, file);
-  }
-
-  private IFile createMockFile(IFolder parent) {
-    IFile file = EasyMock.createMock(IFile.class);
-    EasyMock.expect(file.getParent()).andReturn(parent);
-    return file;
+    element.setParentFolder(parent);
+    assertCloserIsClosed(true);
   }
 
   // Going with the earlier names, I would have loved to call this "unborn children".
@@ -73,36 +85,32 @@ public class PlotElementCloseHandlerTest {
   @Test
   public void closesEditorHandlingUnsavedGrandchild() throws Exception {
     EasyMock.expect(input.getAdapter(IFileEditorInput.class)).andReturn(null);
+    EasyMock.expect(reference.getEditor(false)).andReturn(null);
     DummyPlotElementViewElement child = new DummyPlotElementViewElement(element.getPlotElement());
     element.getPlotElement().addChild(child.getPlotElement(), 0);
     element.addChild(child);
     EasyMock.expect(input.getAdapter(IPlotChild.class))
         .andReturn(new PlotPartPlotChild(child.getPlotElement()))
         .anyTimes();
-    EasyMock.expect(reference.getEditor(false)).andReturn(null);
     assertCloserIsClosed(true);
   }
 
   @Test
   public void doesNotCloseEditorsByNameAlone() throws Exception {
     EasyMock.expect(input.getAdapter(IFileEditorInput.class)).andReturn(input).anyTimes();
-    IFile file = createMockFile(null);
-    EasyMock.expect(input.getFile()).andReturn(file);
-    element.setName("DisplayName"); //$NON-NLS-1$
+    EasyMock.expect(input.getFile()).andReturn(createMockFile(null));
     EasyMock.expect(reference.getName()).andReturn("DisplayName").anyTimes(); //$NON-NLS-1$
-    assertCloserIsClosed(false, file);
+    element.setName("DisplayName"); //$NON-NLS-1$
+    assertCloserIsClosed(false);
   }
 
   @Test
   public void closesEditorHandlingChild() throws Exception {
     EasyMock.expect(input.getAdapter(IFileEditorInput.class)).andReturn(input).anyTimes();
     EasyMock.expect(input.getFile()).andReturn(editorFile).anyTimes();
-
     EasyMock.expect(reference.getEditor(false)).andReturn(null);
-
-    DummyPlotElementViewElement child = new DummyPlotElementViewElement();
-    child.setParentFolder(parentFolder);
-    element.addChild(child);
+    
+    addDummyViewElementTo(element);
     assertCloserIsClosed(true);
   }
 
@@ -110,30 +118,22 @@ public class PlotElementCloseHandlerTest {
   public void closesEditorHandlingYoungerChild() throws Exception {
     EasyMock.expect(input.getAdapter(IFileEditorInput.class)).andReturn(input).anyTimes();
     EasyMock.expect(input.getFile()).andReturn(editorFile).anyTimes();
-
     EasyMock.expect(reference.getEditor(false)).andReturn(null);
-
+    
     element.addChild(new DummyPlotElementViewElement());
-    DummyPlotElementViewElement secondChild = new DummyPlotElementViewElement();
-    secondChild.setParentFolder(parentFolder);
-    element.addChild(secondChild);
+    addDummyViewElementTo(element);
     assertCloserIsClosed(true);
   }
-
+  
   @Test
   public void closesEditorHandlingGrandchild() throws Exception {
     EasyMock.expect(input.getAdapter(IFileEditorInput.class)).andReturn(input).anyTimes();
     EasyMock.expect(input.getFile()).andReturn(editorFile).anyTimes();
-
     EasyMock.expect(reference.getEditor(false)).andReturn(null);
-
     EasyMock.expect(reference.getName()).andReturn("GrandChild").anyTimes(); //$NON-NLS-1$
-    DummyPlotElementViewElement grandchild = new DummyPlotElementViewElement();
-    grandchild.setParentFolder(parentFolder);
-    DummyPlotElementViewElement child = new DummyPlotElementViewElement();
-    child.setParentFolder(parentFolder);
-    element.addChild(child);
-    child.addChild(grandchild);
+
+    DummyPlotElementViewElement child = addDummyViewElementTo(element);
+    addDummyViewElementTo(child);
     assertCloserIsClosed(true);
   }
 }
