4296e2108180ca98fddd05dae8f41809c80b6292
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-07-28 11:01:04 +0000

    Verschieberei

---
+++ Character_Attributes/src/net/sf/anathema/character/attributes/points/AttributeBonusPointCalculator.java
@@ -0,0 +1,15 @@
+package net.sf.anathema.character.attributes.points;
+
+import net.sf.anathema.character.attributes.model.IAttributes;
+
+public class AttributeBonusPointCalculator {
+
+  public AttributeBonusPointCalculator(IAttributes attributes) {
+    // TODO Auto-generated constructor stub
+  }
+
+  public int calculate() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+}
---
+++ Character_Attributes/src/net/sf/anathema/character/attributes/points/AttributeBonusPointHandler.java
@@ -0,0 +1,18 @@
+package net.sf.anathema.character.attributes.points;
+
+import net.sf.anathema.basics.eclipse.extension.AbstractExecutableExtension;
+import net.sf.anathema.character.attributes.model.IAttributes;
+import net.sf.anathema.character.core.model.ICharacterId;
+import net.sf.anathema.character.core.model.IPointHandler;
+import net.sf.anathema.character.core.model.ModelCache;
+import net.sf.anathema.character.core.model.ModelIdentifier;
+
+public class AttributeBonusPointHandler extends AbstractExecutableExtension implements IPointHandler {
+
+  @Override
+  public int getPoints(ICharacterId characterId) {
+    ModelIdentifier identifier = new ModelIdentifier(characterId, IAttributes.MODEL_ID);
+    IAttributes attributes = (IAttributes) ModelCache.getInstance().getModel(identifier);
+    return new AttributeBonusPointCalculator(attributes).calculate();
+  }
+}
