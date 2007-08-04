9f0b5a9e93d8ba58cb1bd26ced6e61cb1b5b0dd6
Urs Reupke <ursreupke@gmail.com>
2007-08-04 11:02:30 +0000

    Fixed i18n

--- Character_Description/src/net/sf/anathema/character/description/Messages.java
+++ Character_Description/src/net/sf/anathema/character/description/Messages.java
@@ -3,7 +3,7 @@ package net.sf.anathema.character.description;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-  private static final String BUNDLE_NAME = "net.sf.anathema.character.description.model.messages"; //$NON-NLS-1$
+  private static final String BUNDLE_NAME = "net.sf.anathema.character.description.messages"; //$NON-NLS-1$
   public static String CharacterDescriptionEditor_Characterization;
   public static String CharacterDescriptionEditor_Concept;
   public static String CharacterDescriptionEditor_Name;
