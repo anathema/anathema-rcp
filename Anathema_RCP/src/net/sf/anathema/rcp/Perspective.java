c43650d59bc49d0a80bd3e84b62ea78edebd3cc6
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-08-05 07:56:40 +0000

    Leerer Freebies-View in Perspektive eingebaut

--- Anathema_RCP/src/net/sf/anathema/rcp/Perspective.java
+++ Anathema_RCP/src/net/sf/anathema/rcp/Perspective.java
@@ -1,19 +1,22 @@
 package net.sf.anathema.rcp;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
 
   public void createInitialLayout(IPageLayout layout) {
-    String editorArea = layout.getEditorArea();
+    String editorAreaReference = layout.getEditorArea();
     layout.setEditorAreaVisible(true);
     layout.setFixed(false);
     // TODO Konstante ID nicht mehrfach deklarieren. Abhängigkeit dennoch vermeiden!
     // TODO Verschiedene Perspektiven bereistellen
     String id = "net.sf.anathema.basics.repositoryview"; //$NON-NLS-1$
     layout.addShowViewShortcut(id);
-    layout.addStandaloneView(id, true, IPageLayout.LEFT, 0.25f, editorArea);
-    layout.addStandaloneView("net.sf.anathema.character.points", true, IPageLayout.BOTTOM, 0.8f, editorArea); //$NON-NLS-1$
+    layout.addStandaloneView(id, true, IPageLayout.LEFT, 0.25f, editorAreaReference);
+    IFolderLayout characterFolder = layout.createFolder("folder.character", IPageLayout.BOTTOM, 0.8f, editorAreaReference); //$NON-NLS-1$
+    characterFolder.addView("net.sf.anathema.character.points"); //$NON-NLS-1$
+    characterFolder.addView("net.sf.anathema.character.freebies"); //$NON-NLS-1$
   }
 }
---
+++ Character_Freebies/src/net/sf/anathema/character/freebies/FreebiesView.java
@@ -0,0 +1,20 @@
+package net.sf.anathema.character.freebies;
+
+import net.sf.anathema.basics.eclipse.ui.DisposableViewPart;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class FreebiesView extends DisposableViewPart {
+
+  @Override
+  public void createPartControl(Composite parent) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void setFocus() {
+    // TODO Auto-generated method stub
+
+  }
+}
