cd9adcd9761e06a5d4f9045f8f4b71160451c23f
Urs Reupke <ursreupke@gmail.com>
2007-08-12 10:47:17 +0000

    tests for charactermodelelement equals

--- Character_Core/test/net/sf/anathema/character/core/repository/internal/CharacterModelViewElementTest.java
+++ Character_Core/test/net/sf/anathema/character/core/repository/internal/CharacterModelViewElementTest.java
@@ -4,18 +4,49 @@ import static org.junit.Assert.*;
 import net.sf.anathema.basics.eclipse.ui.IEditorInputProvider;
 import net.sf.anathema.basics.repository.treecontent.deletion.IPageDelible;
 
+import org.easymock.EasyMock;
+import org.eclipse.core.resources.IFolder;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CharacterModelViewElementTest {
 
+  private CharacterModelViewElement element;
+  private IFolder folder;
+
+  @Before
+  public void createElement() {
+    this.folder = EasyMock.createMock(IFolder.class);
+    this.element = new CharacterModelViewElement(null, folder, null);
+  }
+
   @Test
   public void doesNotAdaptToDelible() {
-    assertNull(new CharacterModelViewElement(null, null, null).getAdapter(IPageDelible.class));
+    assertNull(element.getAdapter(IPageDelible.class));
   }
 
   @Test
   public void providesEditorInput() {
-    assertTrue(new CharacterModelViewElement(null, null, null).getAdapter(IEditorInputProvider.class) instanceof IEditorInputProvider);
+    assertTrue(element.getAdapter(IEditorInputProvider.class) instanceof IEditorInputProvider);
   }
 
+  @Test
+  public void doesNotEqualObject() throws Exception {
+    assertFalse(element.equals(new Object()));
+  }
+
+  @Test
+  public void doesNotEqualOtherCharacterModelElementWithoutFolder() throws Exception {
+    assertFalse(element.equals(new CharacterModelViewElement(null, null, null)));
+  }
+  
+  @Test
+  public void equalsSelf() throws Exception {
+    assertTrue(element.equals(element));
+  }
+  
+  @Test
+  public void equalsOtherElementWithSameFolder() throws Exception {
+    assertTrue(element.equals(new CharacterModelViewElement(null, folder, null)));
+  }
 }
