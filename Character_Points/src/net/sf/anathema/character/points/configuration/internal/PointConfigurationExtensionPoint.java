4b12812c181e1f1557ee39e2630397e3339afba4
Urs Reupke <ursreupke@gmail.com>
2007-08-18 13:34:13 +0000

    Refactoring: IPointHandler einzeln anmeldbar

--- Character_Points/src/net/sf/anathema/character/points/configuration/internal/PointConfigurationExtensionPoint.java
+++ Character_Points/src/net/sf/anathema/character/points/configuration/internal/PointConfigurationExtensionPoint.java
@@ -14,45 +14,50 @@ import net.sf.anathema.character.core.model.ICharacterId;
 import net.sf.anathema.character.core.template.ICharacterTemplate;
 import net.sf.anathema.character.core.template.ICharacterTemplateProvider;
 import net.sf.anathema.character.points.configuration.IPointHandler;
-import net.sf.anathema.character.points.configuration.NullPointHandler;
 import net.sf.anathema.character.points.plugin.PointPluginConstants;
 import net.sf.anathema.lib.collection.MultiEntryMap;
 
 public class PointConfigurationExtensionPoint implements IPointConfigurationProvider {
 
-  private static final String EXTENSION_POINT_ID = "net.sf.anathema.character.points.configuration"; //$NON-NLS-1$
-  private static final String ATTRIB_NAME = "name"; //$NON-NLS-1$
-  private static final String ATTRIB_EXPERIENCE_POINT_CALCULATOR = "experiencePointCalculator"; //$NON-NLS-1$
-  private static final String ATTRIB_BONUS_POINT_CALCULATOR = "bonusPointCalculator"; //$NON-NLS-1$
+  private static final String ATTRIB_GROUP_ID = "groupId"; //$NON-NLS-1$
+  private static final String ATTRIB_GROUP_REFERENCE = "groupReference"; //$NON-NLS-1$
+  private static final String ATTRIB_HANDLER_CLASS = "handlerClass"; //$NON-NLS-1$
   private static final String ATTRIB_MODEL_ID = "modelId"; //$NON-NLS-1$
+  private static final String ATTRIB_NAME = "name"; //$NON-NLS-1$
+  private static final String TAG_BONUS_POINT_CALCULATOR = "bonusPointCalculator"; //$NON-NLS-1$
+  private static final String TAG_EXPERIENCE_POINT_CALCULATOR = "experiencePointCalculator"; //$NON-NLS-1$
+  private static final String TAG_POINT_GROUP = "pointGroup"; //$NON-NLS-1$
+  private static final String EXTENSION_POINT_ID = "net.sf.anathema.character.points.configuration"; //$NON-NLS-1$
   private static final Logger logger = new Logger(PointPluginConstants.PLUGIN_ID);
 
   public IPointConfiguration[] getExperiencePointConfigurations(
       ICharacterTemplateProvider provider,
       ICharacterId characterId) {
-    return getPointConfigurations(provider, characterId, ATTRIB_EXPERIENCE_POINT_CALCULATOR);
+    return getPointConfigurations(provider, characterId, TAG_EXPERIENCE_POINT_CALCULATOR);
   }
 
   public IPointConfiguration[] getBonusPointConfigurations(ICharacterTemplateProvider provider, ICharacterId characterId) {
-    return getPointConfigurations(provider, characterId, ATTRIB_BONUS_POINT_CALCULATOR);
+    return getPointConfigurations(provider, characterId, TAG_BONUS_POINT_CALCULATOR);
   }
 
   private IPointConfiguration[] getPointConfigurations(
       ICharacterTemplateProvider provider,
       ICharacterId characterId,
-      String pointCalculatorAttribute) {
+      String pointCalculatorElementName) {
     ICharacterTemplate template = provider.getTemplate(characterId);
     MultiEntryMap<String, IPointHandler> handlersByGroupId = new MultiEntryMap<String, IPointHandler>();
     Map<String, PointConfiguration> configurationsByGroupId = new HashMap<String, PointConfiguration>();
     for (IPluginExtension extension : getPluginExtensions()) {
       for (IExtensionElement configurationElement : extension.getElements()) {
-        if ("pointConfiguration".equals(configurationElement.getName())) { //$NON-NLS-1$
-          String groupReference = configurationElement.getAttribute("groupReference"); //$NON-NLS-1$
-          IPointHandler handler = getPointHandler(pointCalculatorAttribute, configurationElement);
-          handlersByGroupId.add(groupReference, handler);
+        if (pointCalculatorElementName.equals(configurationElement.getName())) {
+          String groupReference = configurationElement.getAttribute(ATTRIB_GROUP_REFERENCE);
+          IPointHandler handler = getPointHandler(configurationElement);
+          if (handler != null) {
+            handlersByGroupId.add(groupReference, handler);
+          }
         }
-        else {
-          String groupId =  configurationElement.getAttribute("groupId"); //$NON-NLS-1$
+        else if (TAG_POINT_GROUP.equals(configurationElement.getName())) {
+          String groupId = configurationElement.getAttribute(ATTRIB_GROUP_ID);
           String name = configurationElement.getAttribute(ATTRIB_NAME);
           String modelId = configurationElement.getAttribute(ATTRIB_MODEL_ID);
           if (template.supportsModel(modelId)) {
@@ -74,16 +79,13 @@ public class PointConfigurationExtensionPoint implements IPointConfigurationProv
     return configurations.toArray(new IPointConfiguration[configurations.size()]);
   }
 
-  // todo template support
-  // 
-
-  private IPointHandler getPointHandler(String pointCalculatorAttribute, IExtensionElement configurationElement) {
+  private IPointHandler getPointHandler(IExtensionElement element) {
     try {
-      return configurationElement.getAttributeAsObject(pointCalculatorAttribute, IPointHandler.class);
+      return element.getAttributeAsObject(ATTRIB_HANDLER_CLASS, IPointHandler.class);
     }
     catch (ExtensionException e) {
       logger.error(Messages.PointConfigurationExtensionPoint_CalculatorLoadError, e);
-      return new NullPointHandler();
+      return null;
     }
   }
 
