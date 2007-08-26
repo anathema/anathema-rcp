3bdc901019fa4bc1a820785a7b441d108675a8f3
Urs Reupke <ursreupke@gmail.com>
2007-08-26 20:02:55 +0000

    View-Layout is saved and restored. Open editors are not.

--- Anathema_RCP/src/net/sf/anathema/rcp/ApplicationWorkbenchAdvisor.java
+++ Anathema_RCP/src/net/sf/anathema/rcp/ApplicationWorkbenchAdvisor.java
@@ -1,5 +1,6 @@
 package net.sf.anathema.rcp;
 
+import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
@@ -7,6 +8,12 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
   private static final String PERSPECTIVE_ID = "net.sf.anathema.rcp.AnathemaPerspective"; //$NON-NLS-1$
+
+  @Override
+  public void initialize(IWorkbenchConfigurer configurer) {
+    super.initialize(configurer);
+    configurer.setSaveAndRestore(true);
+  }
 
   @Override
   public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
