790382aa6b32bb1385a72a048039eea706af462d
Urs Reupke <ursreupke@gmail.com>
2007-08-26 19:20:18 +0000

    ToggleButton has correct state after toggling.

--- Character_Experience/src/net/sf/anathema/character/experience/internal/ToggleExperienceCommandHandler.java
+++ Character_Experience/src/net/sf/anathema/character/experience/internal/ToggleExperienceCommandHandler.java
@@ -1,5 +1,6 @@
 package net.sf.anathema.character.experience.internal;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import net.sf.anathema.basics.eclipse.logging.Logger;
@@ -15,26 +16,28 @@ import net.sf.anathema.character.experience.plugin.IExperiencePluginConstants;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.expressions.EvaluationContext;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.commands.IElementUpdater;
+import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.menus.UIElement;
 
 public class ToggleExperienceCommandHandler extends AbstractHandler implements IElementUpdater {
   private static final Logger logger = new Logger(IExperiencePluginConstants.PLUGIN_ID);
   private final ExperiencePersister persister = new ExperiencePersister();
+  private IExperience experience;
 
   @Override
-  public Object execute(ExecutionEvent arg0) throws ExecutionException {
-    EvaluationContext context = (EvaluationContext) arg0.getApplicationContext();
-    IEditorPart editor = (IEditorPart) context.getVariable("activeEditor"); //$NON-NLS-1$
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    IEditorPart editor = HandlerUtil.getActiveEditorChecked(event);
     IModelIdentifier currentIdentifier = (IModelIdentifier) editor.getEditorInput().getAdapter(IModelIdentifier.class);
     if (currentIdentifier == null) {
       return null;
     }
     ModelIdentifier experienceIdentifier = new ModelIdentifier(currentIdentifier.getCharacterId(), IExperience.MODEL_ID);
-    IExperience experience = (IExperience) ModelCache.getInstance().getModel(experienceIdentifier);
+    this.experience = (IExperience) ModelCache.getInstance().getModel(experienceIdentifier);
     experience.setExperienced(!experience.isExperienced());
     IContentHandle content = new ModelExtensionPoint().getModelContent(experienceIdentifier);
     try {
@@ -45,11 +48,14 @@ public class ToggleExperienceCommandHandler extends AbstractHandler implements I
       logger.error(Messages.ToggleExperienceActionDelegate_ErrorSavingModel, e);
       throw new ExecutionException(Messages.ToggleExperienceActionDelegate_ErrorSavingModel, e);
     }
+    IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+    ICommandService commandService = (ICommandService) window.getService(ICommandService.class);
+    commandService.refreshElements(event.getCommand().getId(), new HashMap());
     return null;
   }
 
   @Override
   public void updateElement(UIElement element, Map parameters) {
-    element.setChecked(true);
+    element.setChecked(experience.isExperienced());
   }
 }
