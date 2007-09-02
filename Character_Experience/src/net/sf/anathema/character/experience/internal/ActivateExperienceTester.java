b933cbe19e9c362a008baeba14369ef6d11fd687
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-09-02 14:05:23 +0000

    Dämlicher Tester, der nix macht, außer das Plugin zu aktivieren

---
+++ Character_Experience/src/net/sf/anathema/character/experience/internal/ActivateExperienceTester.java
@@ -0,0 +1,11 @@
+package net.sf.anathema.character.experience.internal;
+
+import org.eclipse.core.expressions.PropertyTester;
+
+public class ActivateExperienceTester extends PropertyTester {
+
+  @Override
+  public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+    return true;
+  }
+}
