af12421af8bdb45b042c74ebb200ca8fac4991f3
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-09-04 13:45:16 +0000

    ICharacterId anstellen des IFolders mitgereicht.

--- Character_Description/src/net/sf/anathema/character/description/DescriptionEditorInputFactory.java
+++ Character_Description/src/net/sf/anathema/character/description/DescriptionEditorInputFactory.java
@@ -1,13 +1,13 @@
 package net.sf.anathema.character.description;
 
 import net.sf.anathema.basics.repository.treecontent.itemtype.IDisplayNameProvider;
+import net.sf.anathema.character.core.model.ICharacterId;
 import net.sf.anathema.character.core.model.IModelProvider;
 import net.sf.anathema.character.core.model.ModelIdentifier;
 import net.sf.anathema.character.core.repository.IEditorInputFactory;
 import net.sf.anathema.lib.exception.PersistenceException;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -18,11 +18,11 @@ public class DescriptionEditorInputFactory implements IEditorInputFactory {
   @Override
   public IEditorInput create(
       IFile modelFile,
-      IFolder characterFolder,
+      ICharacterId characterId,
       ImageDescriptor descriptor,
       IDisplayNameProvider nameProvider,
       IModelProvider modelProvider) throws PersistenceException, CoreException {
-    ModelIdentifier identifier = new ModelIdentifier(characterFolder, ICharacterDescription.MODEL_ID);
+    ModelIdentifier identifier = new ModelIdentifier(characterId, ICharacterDescription.MODEL_ID);
     ICharacterDescription description = (ICharacterDescription) modelProvider.getModel(identifier);
     return new CharacterDescriptionEditorInput(modelFile, descriptor, description);
   }
