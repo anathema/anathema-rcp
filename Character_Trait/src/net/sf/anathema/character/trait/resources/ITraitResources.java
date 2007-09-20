597b370a3b8a0692359bb457940fb17d21dbcd29
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-09-20 11:54:39 +0000

    SolarIcon aus Trait rausgeschmissen

--- Character_Trait/src/net/sf/anathema/character/trait/resources/ITraitResources.java
+++ Character_Trait/src/net/sf/anathema/character/trait/resources/ITraitResources.java
@@ -11,13 +11,11 @@ import org.eclipse.jface.resource.ImageRegistry;
 public interface ITraitResources {
 
   public static final String UNSELECTED_BUTTON = "UnselectedButton"; //$NON-NLS-1$
-  public static final String SELECTED_BUTTON = "SelectedButton"; //$NON-NLS-1$
   public static final String SURPLUS_BUTTON = "SurplusButton"; //$NON-NLS-1$
 
   public static ImageRegistry IMAGE_REGISTRY = new ImageRegistry() {
     {
       put(UNSELECTED_BUTTON, createImageDescriptor("BorderUnselectedButton16.png")); //$NON-NLS-1$
-      put(SELECTED_BUTTON, createImageDescriptor("BorderSolarButton16.png")); //$NON-NLS-1$
       put(SURPLUS_BUTTON, createImageDescriptor("BorderBonusButton16.png")); //$NON-NLS-1$
     }
 
