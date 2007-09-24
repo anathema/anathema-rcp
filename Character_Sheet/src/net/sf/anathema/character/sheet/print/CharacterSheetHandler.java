d61205fcfae1edfcb82a6cfa7663e1bb06c2117a
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-09-24 12:01:41 +0000

    i18n, kleineres refactoring

--- Character_Sheet/src/net/sf/anathema/character/sheet/print/CharacterSheetHandler.java
+++ Character_Sheet/src/net/sf/anathema/character/sheet/print/CharacterSheetHandler.java
@@ -1,12 +1,13 @@
 package net.sf.anathema.character.sheet.print;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 import net.disy.commons.core.io.IOUtilities;
+import net.sf.anathema.basics.eclipse.logging.Logger;
 import net.sf.anathema.basics.swt.file.FileChoosing;
-import net.sf.anathema.character.core.model.ICharacterId;
 import net.sf.anathema.character.core.model.IModelIdentifier;
 import net.sf.anathema.character.core.model.ModelCache;
 import net.sf.anathema.character.sheet.content.ICharacter;
@@ -14,32 +15,48 @@ import net.sf.anathema.character.sheet.content.ICharacter;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class CharacterSheetHandler extends AbstractHandler {
+  private Logger logger = new Logger("net.sf.anathema.character.sheet"); //$NON-NLS-1$
 
   @Override
   public Object execute(ExecutionEvent event) throws ExecutionException {
-    IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
+    Shell shell = HandlerUtil.getActiveShell(event);
     OutputStream outputStream = null;
     try {
-      IModelIdentifier identifier = (IModelIdentifier) editorPart.getEditorInput().getAdapter(IModelIdentifier.class);
-      ICharacterId characterId = identifier.getCharacterId();
-      ICharacter character = new Character(characterId, ModelCache.getInstance());
-      File file = FileChoosing.getPdfFile(null, HandlerUtil.getActiveShell(event));
-      if (file == null) {
+      outputStream = createOutputStream(shell);
+      if (outputStream == null) {
         return null;
       }
-      outputStream = new FileOutputStream(file);
+      IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
+      ICharacter character = createCharacter(editorPart);
       new CharacterSheetPrinter().printReport(character, outputStream);
     }
     catch (Exception e) {
-
+      logger.error(Messages.CharacterSheetHandler_CharacterPdfErrorMessage, e);
+      
     }
     finally {
       IOUtilities.close(outputStream);
     }
     return null;
+  }
+
+  private ICharacter createCharacter(IEditorPart editorPart) {
+    IEditorInput editorInput = editorPart.getEditorInput();
+    IModelIdentifier identifier = (IModelIdentifier) editorInput.getAdapter(IModelIdentifier.class);
+    return new Character(identifier.getCharacterId(), ModelCache.getInstance());
+  }
+
+  private OutputStream createOutputStream(Shell shell) throws FileNotFoundException {
+    File file = FileChoosing.getPdfFile(null, shell);
+    if (file == null) {
+      return null;
+    }
+    return new FileOutputStream(file);
   }
 }
---
+++ Character_Sheet/src/net/sf/anathema/character/sheet/print/Messages.java
@@ -0,0 +1,15 @@
+package net.sf.anathema.character.sheet.print;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+  private static final String BUNDLE_NAME = "net.sf.anathema.character.sheet.print.messages"; //$NON-NLS-1$
+  public static String CharacterSheetHandler_CharacterPdfErrorMessage;
+  static {
+    // initialize resource bundle
+    NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+  }
+
+  private Messages() {
+  }
+}
