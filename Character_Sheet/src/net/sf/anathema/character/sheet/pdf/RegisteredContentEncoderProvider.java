c7194e772b6d9b286896d11b736b0c107a3efb95
Urs Reupke <ursreupke@gmail.com>
2007-09-26 12:59:23 +0000

    Extension Points auf "PluginId/PointId" umgestellt.

--- Character_Sheet/src/net/sf/anathema/character/sheet/pdf/RegisteredContentEncoderProvider.java
+++ Character_Sheet/src/net/sf/anathema/character/sheet/pdf/RegisteredContentEncoderProvider.java
@@ -12,53 +12,64 @@ import net.sf.anathema.character.core.character.IModelContainer;
 import net.sf.anathema.character.sheet.common.IDynamicPdfContentBoxEncoder;
 import net.sf.anathema.character.sheet.common.IPdfContentBoxEncoder;
 import net.sf.anathema.character.sheet.content.IContentEncoderProvider;
+import net.sf.anathema.character.sheet.plugin.CharacterSheetPluginConstants;
 
-public class RegisteredContentEncoderProvider implements IContentEncoderProvider {
+public class RegisteredContentEncoderProvider implements
+		IContentEncoderProvider {
 
-  private static final String ATTRIB_CLASS = "class"; //$NON-NLS-1$
-  private static final String ATTRIB_FIELD_ID = "fieldId"; //$NON-NLS-1$
-  private static final String EXTENSION_ID = "net.sf.anathema.character.sheet.encoders"; //$NON-NLS-1$
-  private final IPluginExtension[] extensions;
-  private final ILogger logger;
+	private static final String ATTRIB_CLASS = "class"; //$NON-NLS-1$
+	private static final String ATTRIB_FIELD_ID = "fieldId"; //$NON-NLS-1$
+	private static final String ENCODERS_EXTENSION_POINT = "encoders"; //$NON-NLS-1$
+	private final IPluginExtension[] extensions;
+	private final ILogger logger;
 
-  public RegisteredContentEncoderProvider() {
-    this(new Logger("net.sf.anathema.character.sheet"), new EclipseExtensionProvider().getExtensions(EXTENSION_ID)); //$NON-NLS-1$
-  }
+	public RegisteredContentEncoderProvider() {
+		this(
+				new Logger("net.sf.anathema.character.sheet"), new EclipseExtensionProvider().getExtensions(CharacterSheetPluginConstants.PLUGIN_ID, ENCODERS_EXTENSION_POINT)); //$NON-NLS-1$
+	}
 
-  public RegisteredContentEncoderProvider(ILogger logger, IPluginExtension... extensions) {
-    this.logger = logger;
-    this.extensions = extensions;
-  }
+	public RegisteredContentEncoderProvider(ILogger logger,
+			IPluginExtension... extensions) {
+		this.logger = logger;
+		this.extensions = extensions;
+	}
 
-  @Override
-  public IPdfContentBoxEncoder getContentEncoder(String encoderName, IModelContainer character) {
-    for (IPluginExtension extension : extensions) {
-      for (IExtensionElement element : extension.getElements()) {
-        String fieldId = element.getAttribute(ATTRIB_FIELD_ID);
-        if (fieldId.equals(encoderName)) {
-          try {
-            return element.getAttributeAsObject(ATTRIB_CLASS, IPdfContentBoxEncoder.class);
-          }
-          catch (ExtensionException e) {
-            logger.error(MessageFormat.format(
-                Messages.RegisteredContentEncoderProvider_RETRIEVING_ENCODER_ERROR_MESSAGE,
-                new Object[] { encoderName }), e);
-          }
-        }
-      }
-    }
-    return null;
-  }
+	@Override
+	public IPdfContentBoxEncoder getContentEncoder(String encoderName,
+			IModelContainer character) {
+		for (IPluginExtension extension : extensions) {
+			for (IExtensionElement element : extension.getElements()) {
+				String fieldId = element.getAttribute(ATTRIB_FIELD_ID);
+				if (fieldId.equals(encoderName)) {
+					try {
+						return element.getAttributeAsObject(ATTRIB_CLASS,
+								IPdfContentBoxEncoder.class);
+					} catch (ExtensionException e) {
+						logger
+								.error(
+										MessageFormat
+												.format(
+														Messages.RegisteredContentEncoderProvider_RETRIEVING_ENCODER_ERROR_MESSAGE,
+														new Object[] { encoderName }),
+										e);
+					}
+				}
+			}
+		}
+		return null;
+	}
 
-  @Override
-  public IDynamicPdfContentBoxEncoder getDynamicContentEncoder(String encoderName, IModelContainer character) {
-    IPdfContentBoxEncoder contentEncoder = getContentEncoder(encoderName, character);
-    if (contentEncoder == null) {
-      return null;
-    }
-    if (contentEncoder instanceof IDynamicPdfContentBoxEncoder) {
-      return (IDynamicPdfContentBoxEncoder) contentEncoder;
-    }
-    return new DefaultHeightDynamicEncoder(contentEncoder);
-  }
+	@Override
+	public IDynamicPdfContentBoxEncoder getDynamicContentEncoder(
+			String encoderName, IModelContainer character) {
+		IPdfContentBoxEncoder contentEncoder = getContentEncoder(encoderName,
+				character);
+		if (contentEncoder == null) {
+			return null;
+		}
+		if (contentEncoder instanceof IDynamicPdfContentBoxEncoder) {
+			return (IDynamicPdfContentBoxEncoder) contentEncoder;
+		}
+		return new DefaultHeightDynamicEncoder(contentEncoder);
+	}
 }
