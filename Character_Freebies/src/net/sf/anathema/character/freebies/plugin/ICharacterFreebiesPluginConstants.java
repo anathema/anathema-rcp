0b3ec505f12a23bce914045fe1e5ad26f6816761
Urs Reupke <ursreupke@gmail.com>
2007-09-26 13:08:12 +0000

    Extension Points auf "PluginId/PointId" umgestellt.

---
+++ Character_Freebies/src/net/sf/anathema/character/freebies/plugin/ICharacterFreebiesPluginConstants.java
@@ -0,0 +1,6 @@
+package net.sf.anathema.character.freebies.plugin;
+
+public interface ICharacterFreebiesPluginConstants {
+
+	public static String PLUGIN_ID = "net.sf.anathema.character.freebies"; //$NON-NLS-1$
+}
