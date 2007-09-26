71fbbf6899b7466c13a7b67082e805a429d6875c
Urs Reupke <ursreupke@gmail.com>
2007-09-26 13:08:05 +0000

    Extension Points auf "PluginId/PointId" umgestellt.

---
+++ Character_Sheet/src/net/sf/anathema/character/sheet/plugin/CharacterSheetPluginConstants.java
@@ -0,0 +1,6 @@
+package net.sf.anathema.character.sheet.plugin;
+
+public interface CharacterSheetPluginConstants {
+
+	public static String PLUGIN_ID = "net.sf.anathema.character.sheet"; //$NON-NLS-1$
+}
