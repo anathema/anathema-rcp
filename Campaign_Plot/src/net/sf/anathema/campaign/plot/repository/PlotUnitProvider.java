d1d7f7f748bb50adc4e83d96875b5fca563a480a
Urs Reupke <ursreupke@gmail.com>
2007-09-26 12:59:44 +0000

    Extension Points auf "PluginId/PointId" umgestellt.

--- Campaign_Plot/src/net/sf/anathema/campaign/plot/repository/PlotUnitProvider.java
+++ Campaign_Plot/src/net/sf/anathema/campaign/plot/repository/PlotUnitProvider.java
@@ -7,36 +7,38 @@ import net.disy.commons.core.util.Ensure;
 import net.sf.anathema.basics.eclipse.extension.EclipseExtensionProvider;
 import net.sf.anathema.basics.eclipse.extension.IExtensionElement;
 import net.sf.anathema.basics.eclipse.extension.IPluginExtension;
+import net.sf.anathema.campaign.plot.PlotPlugin;
 
 public class PlotUnitProvider {
 
-  private static final String PLOT_UNIT_EXTENSION_POINT = "net.sf.anathema.campaign.plot.units"; //$NON-NLS-1$
-  private IPlotUnit root;
+	private static final String UNITS_EXTENSION_POINT = "units"; //$NON-NLS-1$
+	private IPlotUnit root;
 
-  public PlotUnitProvider() {
-    Map<String, ExtensionPlotUnit> units = new HashMap<String, ExtensionPlotUnit>();
-    for (IPluginExtension extension : new EclipseExtensionProvider().getExtensions(PLOT_UNIT_EXTENSION_POINT)) {
-      for (IExtensionElement element : extension.getElements()) {
-        ExtensionPlotUnit unit = new ExtensionPlotUnit(extension.getContributorId(), element);
-        String parentId = element.getAttribute("parentId"); //$NON-NLS-1$
-        if (parentId == null) {
-          Ensure.ensureArgumentNull("Only one root unit may be defined.", root); //$NON-NLS-1$
-          this.root = unit;
-        }
-        else {
-          units.get(parentId).setSuccessor(unit);
-        }
-        if (element.getBooleanAttribute("recursive")) { //$NON-NLS-1$
-          unit.setSuccessor(unit);
-        }
-        else {
-          units.put(element.getAttribute("id"), unit); //$NON-NLS-1$
-        }
-      }
-    }
-  }
+	public PlotUnitProvider() {
+		Map<String, ExtensionPlotUnit> units = new HashMap<String, ExtensionPlotUnit>();
+		for (IPluginExtension extension : new EclipseExtensionProvider()
+				.getExtensions(PlotPlugin.ID, UNITS_EXTENSION_POINT)) {
+			for (IExtensionElement element : extension.getElements()) {
+				ExtensionPlotUnit unit = new ExtensionPlotUnit(extension
+						.getContributorId(), element);
+				String parentId = element.getAttribute("parentId"); //$NON-NLS-1$
+				if (parentId == null) {
+					Ensure.ensureArgumentNull(
+							"Only one root unit may be defined.", root); //$NON-NLS-1$
+					this.root = unit;
+				} else {
+					units.get(parentId).setSuccessor(unit);
+				}
+				if (element.getBooleanAttribute("recursive")) { //$NON-NLS-1$
+					unit.setSuccessor(unit);
+				} else {
+					units.put(element.getAttribute("id"), unit); //$NON-NLS-1$
+				}
+			}
+		}
+	}
 
-  public IPlotUnit getRootUnit() {
-    return root;
-  }
+	public IPlotUnit getRootUnit() {
+		return root;
+	}
 }
