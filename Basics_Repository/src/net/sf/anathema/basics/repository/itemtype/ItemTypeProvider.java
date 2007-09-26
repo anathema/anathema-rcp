e6f6b962329cb9992b8c75f01d62a91cc4abda4e
Urs Reupke <ursreupke@gmail.com>
2007-09-26 12:58:36 +0000

    Extension Points auf "PluginId/PointId" umgestellt.

--- Basics_Repository/src/net/sf/anathema/basics/repository/itemtype/ItemTypeProvider.java
+++ Basics_Repository/src/net/sf/anathema/basics/repository/itemtype/ItemTypeProvider.java
@@ -7,37 +7,41 @@ import net.sf.anathema.basics.eclipse.extension.EclipseExtensionProvider;
 import net.sf.anathema.basics.eclipse.extension.IExtensionElement;
 import net.sf.anathema.basics.eclipse.extension.IExtensionProvider;
 import net.sf.anathema.basics.eclipse.extension.IPluginExtension;
+import net.sf.anathema.basics.repository.RepositoryPlugin;
 import net.sf.anathema.basics.repository.itemtype.internal.ItemType;
 
 public class ItemTypeProvider {
 
-  private static final String REPOSITORY_ITEMTYPES = "net.sf.anathema.basics.repository.itemtypes"; //$NON-NLS-1$
-  private final IExtensionProvider configurationProvider;
+	private static final String ITEMTYPES_EXTENSION_POINT = "itemtypes"; //$NON-NLS-1$
+	private final IExtensionProvider configurationProvider;
 
-  public ItemTypeProvider() {
-    this(new EclipseExtensionProvider());
-  }
+	public ItemTypeProvider() {
+		this(new EclipseExtensionProvider());
+	}
 
-  public ItemTypeProvider(IExtensionProvider configurationProvider) {
-    this.configurationProvider = configurationProvider;
-  }
+	public ItemTypeProvider(IExtensionProvider configurationProvider) {
+		this.configurationProvider = configurationProvider;
+	}
 
-  public Iterable<IItemType> getItemTypes() {
-    List<IItemType> itemTypes = new ArrayList<IItemType>();
-    for (IPluginExtension pluginExtension : configurationProvider.getExtensions(REPOSITORY_ITEMTYPES)) {
-      for (IExtensionElement extensionElement : pluginExtension.getElements()) {
-        itemTypes.add(new ItemType(pluginExtension.getContributorId(), extensionElement));
-      }
-    }
-    return itemTypes;
-  }
+	public Iterable<IItemType> getItemTypes() {
+		List<IItemType> itemTypes = new ArrayList<IItemType>();
+		for (IPluginExtension pluginExtension : configurationProvider
+				.getExtensions(RepositoryPlugin.ID, ITEMTYPES_EXTENSION_POINT)) {
+			for (IExtensionElement extensionElement : pluginExtension
+					.getElements()) {
+				itemTypes.add(new ItemType(pluginExtension.getContributorId(),
+						extensionElement));
+			}
+		}
+		return itemTypes;
+	}
 
-  public IItemType getById(String id) {
-    for (IItemType type : getItemTypes()) {
-      if (type.getId().equals(id)) {
-        return type;
-      }
-    }
-    throw new IllegalArgumentException("No item type found for id " + id); //$NON-NLS-1$
-  }
+	public IItemType getById(String id) {
+		for (IItemType type : getItemTypes()) {
+			if (type.getId().equals(id)) {
+				return type;
+			}
+		}
+		throw new IllegalArgumentException("No item type found for id " + id); //$NON-NLS-1$
+	}
 }
