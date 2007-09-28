e64165020270068afe7dfe24783daad3a8e24c09
Urs Reupke <ursreupke@gmail.com>
2007-09-28 16:26:36 +0000

    Removed ridiculous TODO

--- Anathema_RCP/src/net/sf/anathema/rcp/Perspective.java
+++ Anathema_RCP/src/net/sf/anathema/rcp/Perspective.java
@@ -11,11 +11,11 @@ public class Perspective implements IPerspectiveFactory {
     layout.setEditorAreaVisible(true);
     layout.setFixed(false);
     // TODO Konstante ID nicht mehrfach deklarieren. Abhängigkeit dennoch vermeiden!
-    // TODO Verschiedene Perspektiven bereistellen
     String id = "net.sf.anathema.basics.repositoryview"; //$NON-NLS-1$
     layout.addShowViewShortcut(id);
     layout.addStandaloneView(id, true, IPageLayout.LEFT, 0.25f, editorAreaReference);
-    IFolderLayout characterFolder = layout.createFolder("folder.character", IPageLayout.BOTTOM, 0.8f, editorAreaReference); //$NON-NLS-1$
+    IFolderLayout characterFolder = layout.createFolder(
+        "folder.character", IPageLayout.BOTTOM, 0.8f, editorAreaReference); //$NON-NLS-1$
     characterFolder.addView("net.sf.anathema.character.points"); //$NON-NLS-1$
     characterFolder.addView("net.sf.anathema.character.freebies"); //$NON-NLS-1$
   }
