a79c57c185f4c815ed599c293638e3efa4d30573
Urs Reupke <ursreupke@gmail.com>
2007-10-24 19:00:53 +0000

    Basis für ImportWizard

---
+++ Character_Core/src/net/sf/anathema/character/core/importwizard/CharacterImportWizard.java
@@ -0,0 +1,26 @@
+package net.sf.anathema.character.core.importwizard;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class CharacterImportWizard extends Wizard implements IImportWizard {
+
+  public CharacterImportWizard() {
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public boolean performFinish() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void init(IWorkbench workbench, IStructuredSelection selection) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
