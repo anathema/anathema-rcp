eb3c9831d68236cff251e070a5ea3390427e7321
Urs Reupke <ursreupke@gmail.com>
2007-10-27 08:36:17 +0000

    Internal file for characters is created

--- Character_Core/src/net/sf/anathema/character/core/importwizard/CharacterImportWizard.java
+++ Character_Core/src/net/sf/anathema/character/core/importwizard/CharacterImportWizard.java
@@ -4,16 +4,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 import net.disy.commons.core.model.BooleanModel;
+import net.sf.anathema.basics.eclipse.resource.FileUtils;
 import net.sf.anathema.basics.importwizard.AbstractImportWizard;
 import net.sf.anathema.basics.importwizard.FileSelectionStatusFactory;
 import net.sf.anathema.basics.importwizard.FileSelectionWizardPage;
 import net.sf.anathema.basics.importwizard.IFileSelectionModel;
+import net.sf.anathema.basics.repository.access.RepositoryUtilities;
 import net.sf.anathema.basics.repository.itemtype.IItemType;
 import net.sf.anathema.character.core.create.CharacterRepositoryUtilities;
+import net.sf.anathema.character.core.template.CharacterTemplateProvider;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.wizard.IWizardPage;
 
 public class CharacterImportWizard extends AbstractImportWizard {
@@ -29,8 +35,10 @@ public class CharacterImportWizard extends AbstractImportWizard {
 
   @Override
   protected IFile createInternalFile(String filename) throws CoreException {
-    // TODO Auto-generated method stub
-    return null;
+    IProject project = RepositoryUtilities.getProject(CharacterRepositoryUtilities.getCharacterItemType());
+    IFolder characterFolder = FileUtils.createUnusedFolder(project, filename);
+    characterFolder.create(true, true, new NullProgressMonitor());
+    return characterFolder.getFile(CharacterTemplateProvider.TEMPLATE_FILE_NAME);
   }
 
   @Override
