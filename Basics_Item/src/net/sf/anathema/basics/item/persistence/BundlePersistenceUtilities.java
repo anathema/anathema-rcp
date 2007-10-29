a3c2b8be549f8eb4108256ba507575c91595b918
Urs Reupke <ursreupke@gmail.com>
2007-10-29 16:18:23 +0000

    AttributeConverter with correct bundle id

--- Basics_Item/src/net/sf/anathema/basics/item/persistence/BundlePersistenceUtilities.java
+++ Basics_Item/src/net/sf/anathema/basics/item/persistence/BundlePersistenceUtilities.java
@@ -1,5 +1,8 @@
 package net.sf.anathema.basics.item.persistence;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
@@ -9,17 +12,26 @@ import org.osgi.framework.Bundle;
 public class BundlePersistenceUtilities {
 
   private static final String HEADER_BUNDLE_VERSION = "Bundle-Version"; //$NON-NLS-1$
-  private static final String ATTRIB_BUNDLE_VERSION = "bundleVersion"; //$NON-NLS-1$
+  public static final String ATTRIB_BUNDLE_VERSION = "bundleVersion"; //$NON-NLS-1$
 
   public static Document createVersionedDocument(String rootElementName, String bundleId) {
     Element root = DocumentHelper.createElement(rootElementName);
     addBundleVersionAttribute(root, bundleId);
-    Document document = DocumentHelper.createDocument(root);
-    return document;
+    return DocumentHelper.createDocument(root);
   }
 
-  public static void addBundleVersionAttribute(Element element, String bundleId) {
-    Bundle bundle = Platform.getBundle(bundleId);
-    element.addAttribute(ATTRIB_BUNDLE_VERSION, (String) bundle.getHeaders().get(HEADER_BUNDLE_VERSION));
+  public static Map<String, String> getBundleVersionMap(String bundleId) {
+    return Collections.singletonMap(ATTRIB_BUNDLE_VERSION, getBundleVersion(bundleId));
   }
+
+  private static void addBundleVersionAttribute(Element element, String bundleId) {
+    String version = getBundleVersion(bundleId);
+    element.addAttribute(ATTRIB_BUNDLE_VERSION, version);
+  }
+
+  private static String getBundleVersion(String bundleId) {
+    Bundle bundle = Platform.getBundle(bundleId);
+    return (String) bundle.getHeaders().get(HEADER_BUNDLE_VERSION);
+  }
+
 }
--- Character_Core/src/net/sf/anathema/character/core/importwizard/XSLCharacterConverter.java
+++ Character_Core/src/net/sf/anathema/character/core/importwizard/XSLCharacterConverter.java
@@ -1,12 +1,12 @@
 package net.sf.anathema.character.core.importwizard;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Map;
 
 import javax.xml.transform.TransformerException;
 
 import net.sf.anathema.basics.importwizard.XSLDocumentConverter;
+import net.sf.anathema.basics.item.persistence.BundlePersistenceUtilities;
 import net.sf.anathema.character.core.plugin.internal.CharacterCorePlugin;
 
 import org.dom4j.Document;
@@ -16,8 +16,7 @@ public class XSLCharacterConverter {
   private static final String ATTRIBUTE_STYLESHEET = "xsl/AttributeCreation.xsl"; //$NON-NLS-1$
 
   public static Document convertAttributes(Document document) throws TransformerException, IOException {
-    Map<String, String> parameters = Collections.singletonMap("bundleVersion", "1.0.0");
-    Document result = new XSLDocumentConverter(CharacterCorePlugin.ID, ATTRIBUTE_STYLESHEET, parameters).run(document);
-    return result;
+    Map<String, String> parameters = BundlePersistenceUtilities.getBundleVersionMap(CharacterCorePlugin.ID);
+    return new XSLDocumentConverter(CharacterCorePlugin.ID, ATTRIBUTE_STYLESHEET, parameters).run(document);
   }
 }
