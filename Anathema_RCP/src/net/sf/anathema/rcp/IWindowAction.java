65ecad74f4c0a2bc6ef4b678c02711ad3afbadc9
Urs Reupke <ursreupke@gmail.com>
2007-10-31 20:26:48 +0000

    Cleanup

--- Anathema_RCP/src/net/sf/anathema/rcp/IWindowAction.java
+++
@@ -1,9 +0,0 @@
-package net.sf.anathema.rcp;
-
-import org.eclipse.jface.action.IAction;
-import org.eclipse.ui.IWorkbenchWindow;
-
-public interface IWindowAction extends IAction {
-
-  public void setWorkbenchWindow(IWorkbenchWindow window);
-}
