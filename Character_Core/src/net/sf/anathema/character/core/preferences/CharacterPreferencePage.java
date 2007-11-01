ab78f7e44c7ea326ccfef581dec317912270353e
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-11-01 17:38:19 +0000

    Cleanup

--- Character_Core/src/net/sf/anathema/character/core/preferences/CharacterPreferencePage.java
+++ Character_Core/src/net/sf/anathema/character/core/preferences/CharacterPreferencePage.java
@@ -3,7 +3,6 @@ package net.sf.anathema.character.core.preferences;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
@@ -30,14 +29,8 @@ public class CharacterPreferencePage extends PreferencePage implements IWorkbenc
 
   @Override
   protected Control createContents(Composite parent) {
-    initializeDialogUnits(parent);
     Composite composite = new Composite(parent, SWT.NONE);
-    GridLayout layout = new GridLayout();
-    layout.marginHeight = 0;
-    layout.marginWidth = 0;
-    layout.horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
-    layout.verticalSpacing = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
-    composite.setLayout(layout);
+    composite.setLayout(new GridLayout());
     new Label(composite, SWT.NONE).setText("Hasänpreference");
     return composite;
   }
