e4ece19c26dbd905296039914791a3117c6d0ee3
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-11-01 21:43:06 +0000

    Cleanup

--- Character_Trait/test/net/sf/anathema/character/trait/interactive/InteractiveTraitExperiencedTest.java
+++ Character_Trait/test/net/sf/anathema/character/trait/interactive/InteractiveTraitExperiencedTest.java
@@ -22,35 +22,95 @@ public class InteractiveTraitExperiencedTest {
   private Identificate traitType;
   private BasicTrait basicTrait;
   private IInteractiveFavorization favorization;
+  private InteractiveTrait interactiveTrait;
 
   @Before
   public final void createTrait() {
-    IExperience basics = createExperience();
+    IExperience experienced = new DummyExperience();
+    experienced.setExperienced(true);
     this.traitTemplate = new DummyTraitTemplate();
     this.traitType = new Identificate("test"); //$NON-NLS-1$
     this.basicTrait = new BasicTrait(traitType);
     this.favorization = EasyMock.createMock(IInteractiveFavorization.class);
-    ITraitPreferences traitPreferences = createTraitPreferences();
-    this.model = new InteractiveTrait(basicTrait, basics, favorization, traitTemplate, traitPreferences);
-  }
-
-  protected ITraitPreferences createTraitPreferences() {
-    return new DummyTraitPreferences(ExperienceTraitTreatment.LeaveUnchanged);
-  }
-
-  protected final InteractiveTrait getDisplayTrait() {
-    return (InteractiveTrait) model;
+    ITraitPreferences traitPreferences = new DummyTraitPreferences(ExperienceTraitTreatment.LeaveUnchanged);
+    this.interactiveTrait = new InteractiveTrait(basicTrait, experienced, favorization, traitTemplate, traitPreferences);
   }
 
   @Test
-  public void respectsMaximalValueFromRules() throws Exception {
-    this.traitTemplate.setMaximalValue(6);
-    assertEquals(6, getDisplayTrait().getMaximalValue());
+  public void alertsListenersIfExperiencedValueIsFirstSetToCurrentValue() throws Exception {
+    final boolean[] eventReceived = new boolean[] { false };
+    interactiveTrait.addChangeListener(new IChangeListener() {
+      @Override
+      public void stateChanged() {
+        eventReceived[0] = true;
+      }
+    });
+    interactiveTrait.setValue(0);
+    assertTrue(eventReceived[0]);
+  }
+
+  @Test
+  public void favorizationIsDisposedOnDispose() throws Exception {
+    favorization.dispose();
+    EasyMock.replay(favorization);
+    getDisplayTrait().dispose();
+    EasyMock.verify(favorization);
+  }
+
+  protected final InteractiveTrait getDisplayTrait() {
+    return interactiveTrait;
   }
 
   @Test
   public void hasBasicTraitType() throws Exception {
     Assert.assertEquals(traitType, getDisplayTrait().getTraitType());
+  }
+
+  @Test
+  public void hasInitialValue0() throws Exception {
+    assertEquals(0, interactiveTrait.getValue());
+  }
+
+  @Test
+  public void isSilentIfValueRemains() throws Exception {
+    interactiveTrait.setValue(0);
+    final boolean[] eventReceived = new boolean[] { false };
+    interactiveTrait.addChangeListener(new IChangeListener() {
+      @Override
+      public void stateChanged() {
+        eventReceived[0] = true;
+      }
+    });
+    interactiveTrait.setValue(0);
+    assertFalse(eventReceived[0]);
+  }
+
+  @Test
+  public void notifiesListenersIfValueChanges() throws Exception {
+    final boolean[] eventReceived = new boolean[] { false };
+    interactiveTrait.addChangeListener(new IChangeListener() {
+      @Override
+      public void stateChanged() {
+        eventReceived[0] = true;
+      }
+    });
+    interactiveTrait.setValue(1);
+    assertTrue(eventReceived[0]);
+  }
+
+  @Test
+  public void removesListeners() throws Exception {
+    final boolean[] eventReceived = new boolean[] { false };
+    IChangeListener changeListener = new IChangeListener() {
+      @Override
+      public void stateChanged() {
+        eventReceived[0] = true;
+      }
+    };
+    interactiveTrait.addChangeListener(changeListener);
+    interactiveTrait.removeChangeListener(changeListener);
+    interactiveTrait.setValue(1);
+    assertFalse(eventReceived[0]);
   }
 
   @Test
@@ -67,96 +127,14 @@ public class InteractiveTraitExperiencedTest {
   }
 
   @Test
-  public void favorizationIsDisposedOnDispose() throws Exception {
-    favorization.dispose();
-    EasyMock.replay(favorization);
-    getDisplayTrait().dispose();
-    EasyMock.verify(favorization);
-  }
-
-  private boolean experienced = true;
-
-  protected IExperience createExperience() {
-    return new DummyExperience() {
-      @Override
-      public boolean isExperienced() {
-        return experienced;
-      }
-    };
-  }
-
-  @Test
-  public void doesNotChangeCreationValueIfExperiencedValueChanges() throws Exception {
-    model.setValue(5);
-    experienced = false;
-    assertEquals(0, model.getValue());
-  }
-
-  @Test
-  public void alertsListenersIfExperiencedValueIsFirstSetToCurrentValue() throws Exception {
-    final boolean[] eventReceived = new boolean[] { false };
-    model.addChangeListener(new IChangeListener() {
-      @Override
-      public void stateChanged() {
-        eventReceived[0] = true;
-      }
-    });
-    model.setValue(0);
-    assertTrue(eventReceived[0]);
-  }
-
-  protected IIntValueModel model;
-
-  @Test
-  public void hasInitialValue0() throws Exception {
-    assertEquals(0, model.getValue());
+  public void respectsMaximalValueFromRules() throws Exception {
+    this.traitTemplate.setMaximalValue(6);
+    assertEquals(6, getDisplayTrait().getMaximalValue());
   }
 
   @Test
   public void storesValue() throws Exception {
-    model.setValue(1);
-    assertEquals(1, model.getValue());
-  }
-
-  @Test
-  public void notifiesListenersIfValueChanges() throws Exception {
-    final boolean[] eventReceived = new boolean[] { false };
-    model.addChangeListener(new IChangeListener() {
-      @Override
-      public void stateChanged() {
-        eventReceived[0] = true;
-      }
-    });
-    model.setValue(1);
-    assertTrue(eventReceived[0]);
-  }
-
-  @Test
-  public void isSilentIfValueRemains() throws Exception {
-    model.setValue(0);
-    final boolean[] eventReceived = new boolean[] { false };
-    model.addChangeListener(new IChangeListener() {
-      @Override
-      public void stateChanged() {
-        eventReceived[0] = true;
-      }
-    });
-    model.setValue(0);
-    assertFalse(eventReceived[0]);
-  }
-
-  @Test
-  public void removesListeners() throws Exception {
-    final boolean[] eventReceived = new boolean[] { false };
-    IChangeListener changeListener = new IChangeListener() {
-      @Override
-      public void stateChanged() {
-        eventReceived[0] = true;
-      }
-    };
-    model.addChangeListener(changeListener);
-    model.removeChangeListener(changeListener);
-    model.setValue(1);
-    assertFalse(eventReceived[0]);
+    interactiveTrait.setValue(1);
+    assertEquals(1, interactiveTrait.getValue());
   }
 }
