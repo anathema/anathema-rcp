dcffe6dc21f7c3496a48ea3f55e69a9d512f6839
Urs Reupke <ursreupke@gmail.com>
2007-11-08 18:32:37 +0000

    Cleanup

--- Basics_Repository/src/net/sf/anathema/basics/repository/input/internal/Messages.java
+++ Basics_Repository/src/net/sf/anathema/basics/repository/input/internal/Messages.java
@@ -6,10 +6,10 @@ public class Messages extends NLS {
   private static final String BUNDLE_NAME = "net.sf.anathema.basics.repository.input.internal.messages"; //$NON-NLS-1$
   public static String FileItemEditorInputFactory_FileNotFoundMessage;
   static {
-    // initialize resource bundle
     NLS.initializeMessages(BUNDLE_NAME, Messages.class);
   }
 
   private Messages() {
+    // nothing to do
   }
 }
