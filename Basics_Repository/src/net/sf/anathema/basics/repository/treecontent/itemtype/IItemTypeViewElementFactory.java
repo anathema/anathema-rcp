aa1ddbe47e8b5c62fb098781a27841ef41956469
Urs Reupke <ursreupke@gmail.com>
2007-11-14 18:09:20 +0000

    Doc for itemtype extension point

--- Basics_Repository/src/net/sf/anathema/basics/repository/treecontent/itemtype/IItemTypeViewElementFactory.java
+++ Basics_Repository/src/net/sf/anathema/basics/repository/treecontent/itemtype/IItemTypeViewElementFactory.java
@@ -6,9 +6,11 @@ import net.sf.anathema.basics.repository.itemtype.IItemType;
 
 import org.eclipse.core.runtime.IExecutableExtension;
 
-public interface IItemTypeViewElementFactory extends IExecutableExtension{
-  
+public interface IItemTypeViewElementFactory extends IExecutableExtension {
+
+  /** Sets the type of item supported by this factory. This should only be called by the ItemType itself. */
   public void setItemType(IItemType itemType);
 
+  /** Creates the top-level elements for the items of the given item type. */
   public List<IViewElement> createViewElements(IViewElement parent);
 }
