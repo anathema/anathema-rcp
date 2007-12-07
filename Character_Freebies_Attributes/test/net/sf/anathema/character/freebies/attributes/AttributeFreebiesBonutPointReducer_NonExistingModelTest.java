d9f01f807924e873d5f2505c0eed1f55ac0e4ab4
Urs Reupke <ursreupke@gmail.com>
2007-12-07 15:22:14 +0000

    Cleanup

--- Character_Freebies_Attributes/test/net/sf/anathema/character/freebies/attributes/AttributeFreebiesBonutPointReducer_NonExistingModelTest.java
+++ Character_Freebies_Attributes/test/net/sf/anathema/character/freebies/attributes/AttributeFreebiesBonutPointReducer_NonExistingModelTest.java
@@ -1,15 +1,11 @@
 package net.sf.anathema.character.freebies.attributes;
 
-import static org.junit.Assert.*;
-import net.sf.anathema.character.attributes.model.AttributeGroupConfiguration;
-import net.sf.anathema.character.attributes.model.Attributes;
+import static org.junit.Assert.assertEquals;
 import net.sf.anathema.character.core.character.ICharacterId;
 import net.sf.anathema.character.core.character.IModelCollection;
 import net.sf.anathema.character.core.fake.CharacterObjectMother;
 import net.sf.anathema.character.core.model.IModelResourceHandler;
 import net.sf.anathema.character.freebies.attributes.AttributeFreebiesBonusPointReducer_ExistingModelTest.DummyCreditManager;
-import net.sf.anathema.character.trait.collection.ITraitCollectionModel;
-import net.sf.anathema.character.trait.template.EssenceSensitiveTraitTemplate;
 
 import org.easymock.EasyMock;
 import org.junit.Before;
@@ -17,22 +13,17 @@ import org.junit.Test;
 
 public class AttributeFreebiesBonutPointReducer_NonExistingModelTest {
 
-
   private AttributeFreebiesBonusPointReducer reducer;
-  private ITraitCollectionModel attributes;
   private ICharacterId characterId;
 
   @Before
   public void createReducer() throws Exception {
-    this.attributes = Attributes.create(
-        new AttributeGroupConfiguration().getGroups(),
-        new EssenceSensitiveTraitTemplate());
     this.characterId = EasyMock.createMock(ICharacterId.class);
     IModelCollection modelProvider = CharacterObjectMother.createNonLoadingEmptyModelProvider();
     IModelResourceHandler resourceManager = CharacterObjectMother.createEmptyResourceHandler();
     this.reducer = new AttributeFreebiesBonusPointReducer(modelProvider, resourceManager, new DummyCreditManager());
   }
-  
+
   @Test
   public void hasZeroPoints() throws Exception {
     assertEquals(0, reducer.getPoints(characterId));
