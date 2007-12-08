9997b05be89f72928bec5648d5b9d2fd12260d56
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-12-08 14:26:00 +0000

    Jetzt auch ein abstrakter Handler am Start.

---
+++ Character_Report/src/net/sf/anathema/character/report/pdf/AbstractReportHandler.java
@@ -0,0 +1,48 @@
+package net.sf.anathema.character.report.pdf;
+
+import net.sf.anathema.basics.swt.file.PdfFileOutputStreamFactory;
+import net.sf.anathema.character.core.character.ICharacterId;
+import net.sf.anathema.character.core.character.IModelIdentifier;
+import net.sf.anathema.character.core.character.ModelIdentifier;
+import net.sf.anathema.character.core.model.ModelCache;
+import net.sf.anathema.character.core.template.CharacterTemplateProvider;
+import net.sf.anathema.character.description.ICharacterDescription;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.operation.IRunnableContext;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public abstract class AbstractReportHandler extends AbstractHandler {
+
+  protected abstract ICharacterReportWriter createReportWriter();
+
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    Shell shell = HandlerUtil.getActiveShell(event);
+    IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
+    PdfFileOutputStreamFactory outputStreamFactory = new PdfFileOutputStreamFactory(getSuggestedName(editorPart));
+    ICharacterReportWriter reportWriter = createReportWriter();
+    IRunnableContext runnableContext = HandlerUtil.getActiveWorkbenchWindow(event);
+    CharacterReportRunner runner = new CharacterReportRunner(outputStreamFactory, reportWriter);
+    runner.runWriting(shell, editorPart, runnableContext);
+    return null;
+  }
+
+  private String getSuggestedName(IEditorPart editorPart) {
+    IEditorInput editorInput = editorPart.getEditorInput();
+    IModelIdentifier identifier = (IModelIdentifier) editorInput.getAdapter(IModelIdentifier.class);
+    ICharacterId characterId = identifier.getCharacterId();
+    ICharacterDescription description = (ICharacterDescription) ModelCache.getInstance().getModel(
+        new ModelIdentifier(characterId, ICharacterDescription.MODEL_ID));
+    if (description == null || description.getName().isEmpty()) {
+      return new CharacterTemplateProvider().getTemplate(characterId).getName();
+    }
+    return description.getName().getText();
+  }
+
+}
--- Character_Sheet/src/net/sf/anathema/character/sheet/pdf/CharacterSheetPdfHandler.java
+++ Character_Sheet/src/net/sf/anathema/character/sheet/pdf/CharacterSheetPdfHandler.java
@@ -1,51 +1,12 @@
 package net.sf.anathema.character.sheet.pdf;
 
-import net.sf.anathema.basics.swt.file.PdfFileOutputStreamFactory;
-import net.sf.anathema.character.core.character.ICharacterId;
-import net.sf.anathema.character.core.character.IModelIdentifier;
-import net.sf.anathema.character.core.character.ModelIdentifier;
-import net.sf.anathema.character.core.model.ModelCache;
-import net.sf.anathema.character.core.template.CharacterTemplateProvider;
-import net.sf.anathema.character.description.ICharacterDescription;
-import net.sf.anathema.character.report.pdf.CharacterReportRunner;
+import net.sf.anathema.character.report.pdf.AbstractReportHandler;
 import net.sf.anathema.character.report.pdf.ICharacterReportWriter;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.operation.IRunnableContext;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.handlers.HandlerUtil;
-
-public class CharacterSheetPdfHandler extends AbstractHandler {
+public class CharacterSheetPdfHandler extends AbstractReportHandler {
 
   @Override
-  public Object execute(ExecutionEvent event) throws ExecutionException {
-    Shell shell = HandlerUtil.getActiveShell(event);
-    IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
-    PdfFileOutputStreamFactory outputStreamFactory = new PdfFileOutputStreamFactory(getSuggestedName(editorPart));
-    ICharacterReportWriter reportWriter = createReportWriter();
-    IRunnableContext runnableContext = HandlerUtil.getActiveWorkbenchWindow(event);
-    CharacterReportRunner runner = new CharacterReportRunner(outputStreamFactory, reportWriter);
-    runner.runWriting(shell, editorPart, runnableContext);
-    return null;
-  }
-
-  protected CharacterSheetPdfWriter createReportWriter() {
+  protected ICharacterReportWriter createReportWriter() {
     return new CharacterSheetPdfWriter();
-  }
-
-  private String getSuggestedName(IEditorPart editorPart) {
-    IEditorInput editorInput = editorPart.getEditorInput();
-    IModelIdentifier identifier = (IModelIdentifier) editorInput.getAdapter(IModelIdentifier.class);
-    ICharacterId characterId = identifier.getCharacterId();
-    ICharacterDescription description = (ICharacterDescription) ModelCache.getInstance().getModel(
-        new ModelIdentifier(characterId, ICharacterDescription.MODEL_ID));
-    if (description == null || description.getName().isEmpty()) {
-      return new CharacterTemplateProvider().getTemplate(characterId).getName();
-    }
-    return description.getName().getText();
   }
 }
