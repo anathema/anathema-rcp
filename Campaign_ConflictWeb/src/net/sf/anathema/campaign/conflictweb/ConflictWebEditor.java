975a42a0de03ab8ae032758756a18296b431aff1
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2007-12-09 14:55:29 +0000

    Leeres ConflictWeb in Anathema aufzumachen.

---
+++ Campaign_ConflictWeb/src/net/sf/anathema/campaign/conflictweb/ConflictWebEditor.java
@@ -0,0 +1,28 @@
+package net.sf.anathema.campaign.conflictweb;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.gef.DefaultEditDomain;
+import org.eclipse.gef.ui.parts.GraphicalEditor;
+
+public class ConflictWebEditor extends GraphicalEditor {
+  public ConflictWebEditor() {
+    setEditDomain(new DefaultEditDomain(this));
+  }
+
+  @Override
+  protected void configureGraphicalViewer() {
+    super.configureGraphicalViewer();
+    getGraphicalViewer().setEditPartFactory(new ConflictWebEditpartFactory());
+  }
+
+  @Override
+  protected void initializeGraphicalViewer() {
+    // nothing to do
+  }
+
+  @Override
+  public void doSave(IProgressMonitor monitor) {
+    throw new UnsupportedOperationException("No save allowed"); //$NON-NLS-1$
+
+  }
+}
---
+++ Campaign_ConflictWeb/src/net/sf/anathema/campaign/conflictweb/ConflictWebEditpartFactory.java
@@ -0,0 +1,12 @@
+package net.sf.anathema.campaign.conflictweb;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.EditPartFactory;
+
+public class ConflictWebEditpartFactory implements EditPartFactory {
+
+  @Override
+  public EditPart createEditPart(EditPart context, Object model) {
+    return null;
+  }
+}
