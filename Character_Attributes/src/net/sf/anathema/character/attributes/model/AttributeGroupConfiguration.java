fab1818241278874fe2a8ad5aa7df38ef02f2209
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2008-01-13 11:02:50 +0000

    Konstante static gemacht.

--- Character_Attributes/src/net/sf/anathema/character/attributes/model/AttributeGroupConfiguration.java
+++ Character_Attributes/src/net/sf/anathema/character/attributes/model/AttributeGroupConfiguration.java
@@ -6,7 +6,7 @@ import net.sf.anathema.character.trait.template.ITraitTemplate;
 
 public class AttributeGroupConfiguration {
 
-  private final TraitGroup[] groups = new TraitGroup[] {
+  private static final TraitGroup[] groups = new TraitGroup[] {
         new TraitGroup(Messages.AttributeGroup_PhysicalLabel, "Strength", "Dexterity", "Stamina"),  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ 
         new TraitGroup(Messages.AttributeGroup_SocialLabel, "Charisma", "Manipulation", "Appearance"),  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
         new TraitGroup(Messages.AttributeGroup_MentalLabel, "Perception", "Intelligence", "Wits") }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
