f432c4bc8ea83b7d5f487bb2607b8c17b35312bd
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2008-01-27 13:24:52 +0000

    Wieder da.

---
+++ Character_Freebies_Attributes/src/net/sf/anathema/character/freebies/attributes/PrimaryAttributeFreebies.java
@@ -0,0 +1,37 @@
+package net.sf.anathema.character.freebies.attributes;
+
+import net.sf.anathema.basics.eclipse.extension.AbstractExecutableExtension;
+import net.sf.anathema.character.core.character.ICharacterId;
+import net.sf.anathema.character.core.character.IModelCollection;
+import net.sf.anathema.character.freebies.attributes.calculation.AttributePointCalculator;
+import net.sf.anathema.character.freebies.attributes.calculation.AttributePointCalculator.PriorityGroup;
+import net.sf.anathema.character.freebies.configuration.ICreditManager;
+
+public class PrimaryAttributeFreebies extends AbstractExecutableExtension implements IAttributeGroupFreebies {
+
+  private static final String CREDIT_ID = "net.sf.anathema.character.attributes.freebies.primary"; //$NON-NLS-1$
+  private final PrioritylessAttributeFreebies prioritylessAttributeFreebies;
+  private final PriorityGroup priority = AttributePointCalculator.PriorityGroup.Primary;
+
+  public PrimaryAttributeFreebies() {
+    this.prioritylessAttributeFreebies = new PrioritylessAttributeFreebies(priority);
+  }
+
+  public PrimaryAttributeFreebies(IModelCollection modelProvider, ICreditManager creditManager) {
+    this.prioritylessAttributeFreebies = new PrioritylessAttributeFreebies(modelProvider, creditManager, priority);
+  }
+
+  @Override
+  public int getPoints(ICharacterId id, int credit) {
+    return prioritylessAttributeFreebies.getPoints(id, credit);
+  }
+
+  @Override
+  public String getCreditId() {
+    return CREDIT_ID;
+  }
+
+  public PriorityGroup getPriority() {
+    return priority;
+  }
+}
---
+++ Character_Freebies_Attributes/src/net/sf/anathema/character/freebies/attributes/SecondaryAttributeFreebies.java
@@ -0,0 +1,37 @@
+package net.sf.anathema.character.freebies.attributes;
+
+import net.sf.anathema.basics.eclipse.extension.AbstractExecutableExtension;
+import net.sf.anathema.character.core.character.ICharacterId;
+import net.sf.anathema.character.core.character.IModelCollection;
+import net.sf.anathema.character.freebies.attributes.calculation.AttributePointCalculator;
+import net.sf.anathema.character.freebies.attributes.calculation.AttributePointCalculator.PriorityGroup;
+import net.sf.anathema.character.freebies.configuration.ICreditManager;
+
+public class SecondaryAttributeFreebies extends AbstractExecutableExtension implements IAttributeGroupFreebies {
+  private static final String CREDIT_ID = "net.sf.anathema.character.attributes.freebies.secondary"; //$NON-NLS-1$
+  private final PrioritylessAttributeFreebies prioritylessAttributeFreebies;
+  private final PriorityGroup priority = AttributePointCalculator.PriorityGroup.Secondary;
+
+  public SecondaryAttributeFreebies() {
+    this.prioritylessAttributeFreebies = new PrioritylessAttributeFreebies(priority);
+  }
+
+  public SecondaryAttributeFreebies(IModelCollection modelProvider, ICreditManager creditManager) {
+    this.prioritylessAttributeFreebies = new PrioritylessAttributeFreebies(modelProvider, creditManager, priority);
+  }
+
+  @Override
+  public int getPoints(ICharacterId id, int credit) {
+    return prioritylessAttributeFreebies.getPoints(id, credit);
+  }
+
+  @Override
+  public String getCreditId() {
+    return CREDIT_ID;
+  }
+
+  @Override
+  public PriorityGroup getPriority() {
+    return priority;
+  }
+}
