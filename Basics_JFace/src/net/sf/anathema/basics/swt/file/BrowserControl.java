a1a674e41d50e26dd8edac3b936a0a1f33f053f8
Urs Reupke <ursreupke@gmail.com>
2008-02-06 15:39:50 +0000

    Case 76: Vereinfachugn BrowserControl

--- Basics_JFace/src/net/sf/anathema/basics/swt/file/BrowserControl.java
+++ Basics_JFace/src/net/sf/anathema/basics/swt/file/BrowserControl.java
@@ -1,7 +1,6 @@
 package net.sf.anathema.basics.swt.file;
 
 import java.awt.Desktop;
-import java.io.IOException;
 import java.net.URI;
 
 import net.sf.anathema.basics.eclipse.logging.Logger;
@@ -12,32 +11,11 @@ public class BrowserControl {
   private static final String PLUGIN_ID = "net.sf.anathema.basics.jface"; //$NON-NLS-1$
 
   public static void displayUrl(final URI uri) {
-    if (WindowsUtilities.isWindows()) {
-      browseWindows(uri);
-      return;
-    }
-    browseOthers(uri);
-  }
-
-  private static void browseOthers(final URI uri) {
     try {
       Desktop.getDesktop().browse(uri);
     }
     catch (final Exception e) {
-      logThrowableWhileFileOpening(e, uri);
+      new Logger(PLUGIN_ID).error(NLS.bind(Messages.BrowserControl_ErrorOpeningUriFormat, uri), e);
     }
-  }
-
-  private static void browseWindows(final URI uri) {
-    try {
-      WindowsUtilities.executeMsdosCommand("start \"\" \"" + uri + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-    }
-    catch (final IOException e) {
-      logThrowableWhileFileOpening(e, uri);
-    }
-  }
-
-  private static void logThrowableWhileFileOpening(final Throwable throwable, URI uri) {
-    new Logger(PLUGIN_ID).error(NLS.bind(Messages.BrowserControl_ErrorOpeningUriFormat, uri), throwable);
   }
 }
