65c3e8b59b402d314cc4efc23e514d91a2282551
Urs Reupke <ursreupke@gmail.com>
2008-02-17 08:30:40 +0000

    i18n

--- Campaign_Plot_Report/src/net/sf/anathema/campaign/plot/report/Messages.java
+++ Campaign_Plot_Report/src/net/sf/anathema/campaign/plot/report/Messages.java
@@ -6,10 +6,10 @@ public class Messages extends NLS {
   private static final String BUNDLE_NAME = "net.sf.anathema.campaign.note.report.messages"; //$NON-NLS-1$
   public static String PlotExportWizard_Title;
   static {
-    // initialize resource bundle
     NLS.initializeMessages(BUNDLE_NAME, Messages.class);
   }
 
   private Messages() {
+    // nothing to do
   }
 }
