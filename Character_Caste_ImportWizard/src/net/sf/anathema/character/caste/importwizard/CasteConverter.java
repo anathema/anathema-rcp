0c6680c052b53050e8d1b795aba95d5944dddd0f
Urs Reupke <ursreupke@gmail.com>
2008-02-17 09:43:16 +0000

    Infrastruktur für Kastenimport

---
+++ Character_Caste_ImportWizard/src/net/sf/anathema/character/caste/importwizard/CasteConverter.java
@@ -0,0 +1,17 @@
+package net.sf.anathema.character.caste.importwizard;
+
+import java.io.IOException;
+
+import net.sf.anathema.character.importwizard.IDocumentConverter;
+
+import org.dom4j.Document;
+import org.eclipse.core.runtime.CoreException;
+
+public class CasteConverter implements IDocumentConverter {
+
+  @Override
+  public Document convert(Document document) throws CoreException, IOException {
+    // TODO Case 95: Testen & Implementieren
+    return null;
+  }
+}
---
+++ Character_Caste_ImportWizard/src/net/sf/anathema/character/caste/importwizard/CasteImporter.java
@@ -0,0 +1,23 @@
+package net.sf.anathema.character.caste.importwizard;
+
+import java.io.IOException;
+
+import net.sf.anathema.basics.eclipse.extension.AbstractExecutableExtension;
+import net.sf.anathema.character.caste.model.ICasteModel;
+import net.sf.anathema.character.importwizard.IModelImporter;
+import net.sf.anathema.character.importwizard.ModelImporter;
+
+import org.dom4j.Document;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+
+public class CasteImporter extends AbstractExecutableExtension implements IModelImporter {
+
+  private final ModelImporter importer = new ModelImporter(new CasteConverter(), ICasteModel.ID);
+
+  @Override
+  public IStatus runImport(IContainer container, Document document) throws IOException, CoreException {
+    return importer.runImport(container, document);
+  }
+}
