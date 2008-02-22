95ce2e4cccdcec4712768ede763f720b5e36fe78
Urs Reupke <ursreupke@gmail.com>
2008-02-22 21:02:10 +0000

    Case 148: ExportItemPage arbeitet mit Viewer

--- Basics_PdfExport/src/net/sf/anathema/basics/pdfexport/item/ExportItemDialogPage.java
+++ Basics_PdfExport/src/net/sf/anathema/basics/pdfexport/item/ExportItemDialogPage.java
@@ -2,18 +2,39 @@ package net.sf.anathema.basics.pdfexport.item;
 
 import net.disy.commons.core.model.ObjectModel;
 import net.disy.commons.core.model.listener.IChangeListener;
-import net.disy.commons.core.util.ITransformer;
 import net.sf.anathema.basics.pdfexport.writer.IExportItem;
 
+import org.eclipse.jface.viewers.IBaseLabelProvider;
+import org.eclipse.jface.viewers.IContentProvider;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.List;
 
 public class ExportItemDialogPage<T> extends WizardPage {
 
-  public static final class ExportItemToPrintNameTransformer<T> implements ITransformer<IExportItem<T>, String> {
+  private final class ExportItemProvider implements IStructuredContentProvider {
     @Override
-    public String transform(IExportItem<T> exportItem) {
-      return exportItem.getPrintName();
+    public Object[] getElements(Object inputElement) {
+      return exportItems.toArray(new Object[exportItems.size()]);
+    }
+
+    @Override
+    public void dispose() {
+      // nothing to do
+    }
+
+    @Override
+    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+      // nothing to do
     }
   }
 
@@ -36,9 +57,29 @@ public class ExportItemDialogPage<T> extends WizardPage {
 
   @Override
   public void createControl(Composite parent) {
-    ExportItemToPrintNameTransformer<T> transformer = new ExportItemToPrintNameTransformer<T>();
-    TransformingList<IExportItem<T>> list = new TransformingList<IExportItem<T>>(exportItems, transformer, selectedItem);
-    setControl(list.createList(parent));
+    ListViewer viewer = new ListViewer(new List(parent, SWT.SINGLE | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER));
+    IContentProvider provider = new ExportItemProvider();
+    viewer.setContentProvider(provider);
+    IBaseLabelProvider labelProvider = new LabelProvider() {
+      @Override
+      public String getText(Object element) {
+        return ((IExportItem< ? >) element).getPrintName();
+      }
+    };
+    viewer.setLabelProvider(labelProvider);
+    viewer.setInput(exportItems);
+    viewer.addSelectionChangedListener(new ISelectionChangedListener() {
+      @Override
+      public void selectionChanged(SelectionChangedEvent event) {
+        IExportItem<T> selection = (IExportItem<T>) ((IStructuredSelection) event.getSelection()).getFirstElement();
+        selectedItem.setValue(selection);
+      }
+    });
+    IExportItem<T> item = selectedItem.getValue();
+    if (item != null) {
+      viewer.setSelection(new StructuredSelection(item));
+    }
+    setControl(viewer.getControl());
   }
 
   @Override
