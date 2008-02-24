c4a8e1b2d32f56d3e45a4613bb045ef6ff9d349d
Urs Reupke <ursreupke@gmail.com>
2008-02-24 12:22:28 +0000

    Case 160: Additions-Group im Context-Menu

--- Basics_JFace/src/net/sf/anathema/basics/jface/context/ContextMenuManager.java
+++ Basics_JFace/src/net/sf/anathema/basics/jface/context/ContextMenuManager.java
@@ -1,17 +1,27 @@
 package net.sf.anathema.basics.jface.context;
 
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchPartSite;
 
 public class ContextMenuManager {
 
   public static void connect(IWorkbenchPartSite site, Viewer viewer) {
-      MenuManager menuMgr = new MenuManager("#PopupMenu"); //$NON-NLS-1$
-      menuMgr.setRemoveAllWhenShown(true);
-      Menu menu = menuMgr.createContextMenu(viewer.getControl());
-      viewer.getControl().setMenu(menu);
-      site.registerContextMenu(menuMgr, viewer);
+    final MenuManager manager = new MenuManager("#PopupMenu"); //$NON-NLS-1$
+    manager.setRemoveAllWhenShown(true);
+    manager.addMenuListener(new IMenuListener() {
+      @Override
+      public void menuAboutToShow(IMenuManager menuManager) {
+        menuManager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+      }
+    });
+    Menu menu = manager.createContextMenu(viewer.getControl());
+    viewer.getControl().setMenu(menu);
+    site.registerContextMenu(manager, viewer);
   }
 }
