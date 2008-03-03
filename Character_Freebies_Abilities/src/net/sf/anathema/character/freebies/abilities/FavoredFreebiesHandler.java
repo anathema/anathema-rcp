3313a09dc8fddc2d3963de0c6b912bf40bb13ffb
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2008-03-03 11:23:27 +0000

    Case 171: Ability Freeby Credits eingepflegt.

---
+++ Character_Freebies_Abilities/src/net/sf/anathema/character/freebies/abilities/FavoredFreebiesHandler.java
@@ -0,0 +1,19 @@
+package net.sf.anathema.character.freebies.abilities;
+
+import net.sf.anathema.basics.eclipse.extension.AbstractExecutableExtension;
+import net.sf.anathema.character.core.character.ICharacterId;
+import net.sf.anathema.character.freebies.configuration.IFreebiesHandler;
+
+public class FavoredFreebiesHandler extends AbstractExecutableExtension implements IFreebiesHandler {
+
+  @Override
+  public String getCreditId() {
+    return "net.sf.anathema.character.abilties.freebies.favored"; //$NON-NLS-1$
+  }
+
+  @Override
+  public int getPoints(ICharacterId id, int credit) {
+    // TODO: Case 189: Einstiegstelle
+    return 0;
+  }
+}
