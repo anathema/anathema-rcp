32d0e8a58242a8f87204f3144839057c72388406
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2008-03-03 09:10:08 +0000

    Case 186: ExtensionPoint für Ability-Groups

--- Character_Abilities/src/net/sf/anathema/character/abilities/IAbilitiesPluginConstants.java
+++ Character_Abilities/src/net/sf/anathema/character/abilities/IAbilitiesPluginConstants.java
@@ -3,4 +3,5 @@ package net.sf.anathema.character.abilities;
 public interface IAbilitiesPluginConstants {
 
   public static final String MODEL_ID = "net.sf.anathema.character.abilities.model"; //$NON-NLS-1$
+  public static final String PLUGIN_ID = "net.sf.anathema.character.abilities";
 }
--- Character_Abilities/src/net/sf/anathema/character/abilities/model/AbilitiesGroupConfiguration.java
+++ Character_Abilities/src/net/sf/anathema/character/abilities/model/AbilitiesGroupConfiguration.java
@@ -15,32 +15,7 @@ public class AbilitiesGroupConfiguration implements ITraitGroupConfiguration {
   }
 
   public TraitGroup[] getGroups() {
-    return new TraitGroup[] {
-        new TraitGroup(
-            "War", //$NON-NLS-1$
-            "Archery", //$NON-NLS-1$
-            "Athletics", //$NON-NLS-1$
-            "Awareness", //$NON-NLS-1$
-            "Dodge", //$NON-NLS-1$
-            "Integrity", //$NON-NLS-1$
-            "MartialArts", //$NON-NLS-1$
-            "Melee", //$NON-NLS-1$
-            "Resistance", //$NON-NLS-1$
-            "Thrown", //$NON-NLS-1$
-            "War"), //$NON-NLS-1$
-        new TraitGroup(
-            "Life", //$NON-NLS-1$
-            "Craft", //$NON-NLS-1$
-            "Larceny", //$NON-NLS-1$
-            "Linguistics", //$NON-NLS-1$
-            "Performance", //$NON-NLS-1$
-            "Presence", //$NON-NLS-1$
-            "Ride", //$NON-NLS-1$
-            "Sail", //$NON-NLS-1$
-            "Socialize", //$NON-NLS-1$
-            "Stealth", //$NON-NLS-1$
-            "Survival"), //$NON-NLS-1$
-        new TraitGroup("Wisdom", "Bureaucracy", "Investigation", "Lore", "Medicine", "Occult") }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
+    return new AbilityGroupsProvider().getTraitGroups(characterTemplate.getCharacterTypeId());
   }
 
   public ITraitTemplate getTraitTemplate() {
