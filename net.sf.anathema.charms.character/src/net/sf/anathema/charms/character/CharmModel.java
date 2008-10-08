f2ab9df7ce6d5a2111868f422770933697071748
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2008-10-08 11:29:50 +0000

    CAD 282: Oberklasse AbstractModel verwendet.

--- net.sf.anathema.charms.character/src/net/sf/anathema/charms/character/CharmModel.java
+++ net.sf.anathema.charms.character/src/net/sf/anathema/charms/character/CharmModel.java
@@ -1,54 +1,13 @@
 package net.sf.anathema.charms.character;
 
 import net.disy.commons.core.model.listener.IChangeListener;
+import net.sf.anathema.character.core.model.AbstractModel;
+import net.sf.anathema.lib.control.ChangeManagement;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 
-public class CharmModel implements ICharmModel {
-
-  @Override
-  public void updateToDependencies() {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void addDirtyListener(IChangeListener changeListener) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public int getDirtyListenerCount() {
-    // TODO Auto-generated method stub
-    return 0;
-  }
-
-  @Override
-  public boolean isDirty() {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public void removeDirtyListener(IChangeListener changeListener) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void setClean() {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void setInitializationData(IConfigurationElement config, String propertyName, Object data)
-      throws CoreException {
-    // TODO Auto-generated method stub
-
-  }
+public class CharmModel extends AbstractModel implements ICharmModel {
 
   @Override
   public void addChangeListener(IChangeListener arg0) {
@@ -59,7 +18,5 @@ public class CharmModel implements ICharmModel {
   @Override
   public void removeChangeListener(IChangeListener arg0) {
     // TODO Auto-generated method stub
-
   }
-
 }
