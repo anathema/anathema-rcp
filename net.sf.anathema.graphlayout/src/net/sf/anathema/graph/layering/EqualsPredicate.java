73730abfa9dc3649bee24ae8308bfbf365110c9d
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2008-11-09 09:59:54 +0000

    graphlayout für rcp eingecheckt.

---
+++ net.sf.anathema.graphlayout/src/net/sf/anathema/graph/layering/EqualsPredicate.java
@@ -0,0 +1,17 @@
+package net.sf.anathema.graph.layering;
+
+import net.disy.commons.core.predicate.IPredicate;
+
+public class EqualsPredicate<T> implements IPredicate<T> {
+
+  private final T value;
+
+  public EqualsPredicate(T value) {
+    this.value = value;
+  }
+
+  @Override
+  public boolean evaluate(T input) {
+    return value.equals(input);
+  }
+}
