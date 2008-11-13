02284921f81e675f4b94b4a3ffbd159d5662b7cf
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2008-11-13 11:56:08 +0000

    Cleanup

--- net.sf.anathema.character.abilities/src/net/sf/anathema/character/abilities/model/DefaultAbilityGroups.java
+++ net.sf.anathema.character.abilities/src/net/sf/anathema/character/abilities/model/DefaultAbilityGroups.java
@@ -4,7 +4,7 @@ import net.sf.anathema.character.trait.group.TraitGroup;
 
 public class DefaultAbilityGroups {
 
-  private static final TraitGroup[] groups = new TraitGroup[] { new TraitGroup("War", //$NON-NLS-1$
+  private static final TraitGroup WAR_GROUP = new TraitGroup("War", //$NON-NLS-1$
       Messages.DefaultAbilityGroups_War,
       "Archery", //$NON-NLS-1$
       "Athletics", //$NON-NLS-1$
@@ -14,20 +14,26 @@ public class DefaultAbilityGroups {
       "MartialArts", //$NON-NLS-1$
       "Melee", //$NON-NLS-1$
       "Resistance", //$NON-NLS-1$
-      "Thrown", "War"), //$NON-NLS-1$ //$NON-NLS-2$
-      new TraitGroup("Life", //$NON-NLS-1$
-          Messages.DefaultAbilityGroups_Life,
-          "Craft", //$NON-NLS-1$
-          "Larceny", //$NON-NLS-1$
-          "Linguistics", //$NON-NLS-1$
-          "Performance", //$NON-NLS-1$
-          "Presence", //$NON-NLS-1$
-          "Ride", //$NON-NLS-1$
-          "Sail", //$NON-NLS-1$
-          "Socialize", //$NON-NLS-1$
-          "Stealth", "Survival"), //$NON-NLS-1$ //$NON-NLS-2$
-      new TraitGroup(
-          "Wisdom", Messages.DefaultAbilityGroups_Wisdom, "Bureaucracy", "Investigation", "Lore", "Medicine", "Occult") }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
+      "Thrown", "War"); //$NON-NLS-1$ //$NON-NLS-2$
+  private static final TraitGroup LIFE_GROUP = new TraitGroup("Life", //$NON-NLS-1$
+      Messages.DefaultAbilityGroups_Life,
+      "Craft", //$NON-NLS-1$
+      "Larceny", //$NON-NLS-1$
+      "Linguistics", //$NON-NLS-1$
+      "Performance", //$NON-NLS-1$
+      "Presence", //$NON-NLS-1$
+      "Ride", //$NON-NLS-1$
+      "Sail", //$NON-NLS-1$
+      "Socialize", //$NON-NLS-1$
+      "Stealth", "Survival"); //$NON-NLS-1$ //$NON-NLS-2$
+  private static final TraitGroup WISDOM_GROUP = new TraitGroup("Wisdom", //$NON-NLS-1$
+      Messages.DefaultAbilityGroups_Wisdom,
+      "Bureaucracy", //$NON-NLS-1$
+      "Investigation", //$NON-NLS-1$
+      "Lore", //$NON-NLS-1$
+      "Medicine", //$NON-NLS-1$
+      "Occult"); //$NON-NLS-1$
+  private static final TraitGroup[] groups = new TraitGroup[] { WAR_GROUP, LIFE_GROUP, WISDOM_GROUP };
 
   public TraitGroup[] get() {
     return groups;
