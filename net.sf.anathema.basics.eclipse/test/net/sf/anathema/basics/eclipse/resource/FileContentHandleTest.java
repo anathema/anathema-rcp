b44f4bbcb99cdfe622c5232cc4bf21da0825c4d3
Urs Reupke <ursreupke@gmail.com>
2009-01-17 10:27:18 +0000

    Cleanup

--- net.sf.anathema.basics.eclipse/test/net/sf/anathema/basics/eclipse/resource/FileContentHandleTest.java
+++ net.sf.anathema.basics.eclipse/test/net/sf/anathema/basics/eclipse/resource/FileContentHandleTest.java
@@ -6,6 +6,7 @@ import org.easymock.EasyMock;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.junit.Test;
 
 public class FileContentHandleTest {
@@ -13,19 +14,30 @@ public class FileContentHandleTest {
   @Test
   public void isAdaptableForMarkerHandle() throws Exception {
     IMarker marker = EasyMock.createMock(IMarker.class);
-    IFile file = EasyMock.createMock(IFile.class);
-    EasyMock.expect(file.createMarker("markerID")).andReturn(marker); //$NON-NLS-1$
-    EasyMock.replay(file);
+    String markerId = "markerID";
+    IFile file = createMarkedFile(marker, markerId);
     FileContentHandle contentHandle = new FileContentHandle(file);
     IMarkerHandle markerHandler = (IMarkerHandle) contentHandle.getAdapter(IMarkerHandle.class);
-    assertSame(marker, markerHandler.createMarker("markerID")); //$NON-NLS-1$
+    assertSame(marker, markerHandler.createMarker(markerId)); //$NON-NLS-1$
   }
-  
+
+  private IFile createMarkedFile(IMarker marker, String markerId) throws CoreException {
+    IFile file = EasyMock.createMock(IFile.class);
+    EasyMock.expect(file.createMarker(markerId)).andReturn(marker); //$NON-NLS-1$
+    EasyMock.replay(file);
+    return file;
+  }
+
   @Test
   public void isAdaptableForResource() throws Exception {
-    IFile file = EasyMock.createMock(IFile.class);
-    EasyMock.replay(file);
+    IFile file = createFile();
     FileContentHandle contentHandle = new FileContentHandle(file);
     assertSame(file, contentHandle.getAdapter(IResource.class));
+  }
+
+  private IFile createFile() {
+    IFile file = EasyMock.createMock(IFile.class);
+    EasyMock.replay(file);
+    return file;
   }
 }
