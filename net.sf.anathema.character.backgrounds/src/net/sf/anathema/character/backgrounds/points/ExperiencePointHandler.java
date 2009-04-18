2844a2f396f37e8f9b19e40ea2dc175ecef33fa0
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2009-04-18 09:02:21 +0000

    Case 414: ExperiencePointHandler für Backgrounds angemeldet.

---
+++ net.sf.anathema.character.backgrounds/src/net/sf/anathema/character/backgrounds/points/ExperiencePointHandler.java
@@ -0,0 +1,23 @@
+package net.sf.anathema.character.backgrounds.points;
+
+import net.sf.anathema.character.backgrounds.model.IBackgroundModel;
+import net.sf.anathema.character.core.character.ICharacterId;
+import net.sf.anathema.character.points.configuration.AbstractPointHandler;
+import net.sf.anathema.character.trait.IBasicTrait;
+import net.sf.anathema.character.trait.collection.ITraitCollectionModel;
+import net.sf.anathema.character.trait.points.ICalculator;
+import net.sf.anathema.character.trait.points.PointWiseExperienceCalculator;
+
+public class ExperiencePointHandler extends AbstractPointHandler<ITraitCollectionModel> {
+
+  public ExperiencePointHandler() {
+    super(IBackgroundModel.MODEL_ID);
+  }
+
+  @Override
+  protected int calculatePoints(final ITraitCollectionModel traits, final ICharacterId characterId) {
+    Iterable<IBasicTrait> backgrounds = traits.getAllTraits();
+    ICalculator calculator = new PointWiseExperienceCalculator(3, backgrounds);
+    return calculator.calculate();
+  }
+}
--- net.sf.anathema.charactertype.solar.acceptance/platformtest/charactertype/solar/acceptance/SolarBackground_Test.java
+++ net.sf.anathema.charactertype.solar.acceptance/platformtest/charactertype/solar/acceptance/SolarBackground_Test.java
@@ -3,6 +3,7 @@ package charactertype.solar.acceptance;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import net.sf.anathema.character.backgrounds.model.IBackgroundModel;
+import net.sf.anathema.character.trait.IBasicTrait;
 
 import org.junit.Test;
 
@@ -23,7 +24,20 @@ public class SolarBackground_Test extends AbstractDefaultSolarTest {
     assertSpentsBonusBackground(7);
   }
 
+  @Test
+  public void spents6XpOnIncreasingBackgroundByTwoOnExperience() throws Exception {
+    IBackgroundModel model = (IBackgroundModel) character.getModel(IBackgroundModel.MODEL_ID);
+    IBasicTrait background = model.addBackground("Resources");
+    background.getCreationModel().setValue(3);
+    background.getExperiencedModel().setValue(5);
+    assertSpentsXpOnBackgrounds(6);
+  }
+
   private void assertSpentsBonusBackground(int expectedPoints) throws Exception {
     assertThat(character.getBonusPoints(BACKGROUND_CONFIGURATION), is(expectedPoints));
+  }
+
+  private void assertSpentsXpOnBackgrounds(int expectedPoints) throws Exception {
+    assertThat(character.getXpSpent(BACKGROUND_CONFIGURATION), is(expectedPoints));
   }
 }
