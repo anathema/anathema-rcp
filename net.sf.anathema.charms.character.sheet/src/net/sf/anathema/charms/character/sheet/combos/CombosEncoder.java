12b5913c866632c6b1c36018c5de16a78309a504
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2009-05-07 16:58:08 +0000

    Case 441: Konkrete Unterklassen des LineEncoders für die leeren Boxen angemeldet.

---
+++ net.sf.anathema.charms.character.sheet/src/net/sf/anathema/charms/character/sheet/combos/CombosEncoder.java
@@ -0,0 +1,10 @@
+package net.sf.anathema.charms.character.sheet.combos;
+
+import net.sf.anathema.character.sheet.content.DynamicPdfHorizontalLineContentEncoder;
+
+public class CombosEncoder extends DynamicPdfHorizontalLineContentEncoder {
+
+  public CombosEncoder() {
+    super("Combos");
+  }
+}
