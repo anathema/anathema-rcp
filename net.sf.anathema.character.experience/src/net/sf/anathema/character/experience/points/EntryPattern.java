97969efd4d05784423df76d74188bd620c07ec2d
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2009-05-12 19:17:04 +0000

    Case 443:  Cleanup.

--- net.sf.anathema.character.experience/src/net/sf/anathema/character/experience/points/EntryPattern.java
+++ net.sf.anathema.character.experience/src/net/sf/anathema/character/experience/points/EntryPattern.java
@@ -1,12 +1,15 @@
 package net.sf.anathema.character.experience.points;
 
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EntryPattern {
 
-  private final String pattern;
+  private final String patternString;
   private final int pointGroup;
   private final int commentGroup;
+  private CharSequence trimmedText;
+  private Matcher matcher;
 
   public static EntryPattern CreatePointsFront(String pattern) {
     return new EntryPattern(pattern, 1, 2);
@@ -17,21 +20,23 @@ public class EntryPattern {
   }
 
   private EntryPattern(String pattern, int pointGroup, int commentGroup) {
-    this.pattern = pattern;
+    this.patternString = pattern;
     this.pointGroup = pointGroup;
     this.commentGroup = commentGroup;
   }
 
-  public String getPattern() {
-    return pattern;
-  }
-
-  public int getPoints(Matcher matcher) {
+  public int getPoints() {
     String pointString = matcher.group(pointGroup);
     return Integer.valueOf(pointString);
   }
 
-  public String getComment(Matcher matcher) {
-    return matcher.group(commentGroup);
+  public String getComment() {
+    return matcher.group(commentGroup).trim();
+  }
+
+  public boolean matches(String text) {
+    Pattern pattern = Pattern.compile(patternString);
+    matcher = pattern.matcher(text);
+    return matcher.matches();
   }
 }
--- net.sf.anathema.character.experience/src/net/sf/anathema/character/experience/points/ExperienceEntryParser.java
+++ net.sf.anathema.character.experience/src/net/sf/anathema/character/experience/points/ExperienceEntryParser.java
@@ -2,8 +2,6 @@ package net.sf.anathema.character.experience.points;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ExperienceEntryParser {
 
@@ -20,14 +18,16 @@ public class ExperienceEntryParser {
       throw new IllegalArgumentException("Empty String not allowed.");
     }
     for (EntryPattern entryPattern : patterns) {
-      Pattern pattern = Pattern.compile(entryPattern.getPattern());
-      Matcher matcher = pattern.matcher(trimmedText);
-      if (matcher.matches()) {
-        String commentString = entryPattern.getComment(matcher);
-        int points = entryPattern.getPoints(matcher);
-        return ExperienceEntry.CreateForPointsAndComment(points, commentString.trim());
+      if (entryPattern.matches(trimmedText)) {
+        return createEntryFrom(entryPattern);
       }
     }
     return ExperienceEntry.CreateForPointsAndComment(4, trimmedText);
+  }
+
+  private ExperienceEntry createEntryFrom(EntryPattern pattern) {
+    String comment = pattern.getComment();
+    int points = pattern.getPoints();
+    return ExperienceEntry.CreateForPointsAndComment(points, comment);
   }
 }
