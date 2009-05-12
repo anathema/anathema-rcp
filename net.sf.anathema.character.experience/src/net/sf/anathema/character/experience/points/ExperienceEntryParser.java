8bc74ac213ef38677dbe11624c2584db03e67bf4
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2009-05-12 18:37:48 +0000

    Case 443:  Parser über regulären Ausdruck.

--- net.sf.anathema.character.experience/src/net/sf/anathema/character/experience/points/ExperienceEntryParser.java
+++ net.sf.anathema.character.experience/src/net/sf/anathema/character/experience/points/ExperienceEntryParser.java
@@ -1,5 +1,8 @@
 package net.sf.anathema.character.experience.points;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ExperienceEntryParser {
 
   private final static String FRONT_POINT_PATTERN = "(-?\\d*)\\s*(.*)";
@@ -9,23 +12,12 @@ public class ExperienceEntryParser {
     if (trimmedText.isEmpty()) {
       throw new IllegalArgumentException("Empty String not allowed.");
     }
-    int points = 4;
-    String[] tokens = trimmedText.split(" "); //$NON-NLS-1$
-    String comment = trimmedText;
-    if (isInteger(tokens[0])) {
-      points = Integer.valueOf(tokens[0]);
-      comment = trimmedText.substring(tokens[0].length() + 1);
-    }
-    return ExperienceEntry.CreateForPointsAndComment(points, comment);
-  }
-
-  private boolean isInteger(String token) {
-    try {
-      Integer.parseInt(token);
-    }
-    catch (NumberFormatException e) {
-      return false;
-    }
-    return true;
+    Pattern pattern = Pattern.compile(FRONT_POINT_PATTERN);
+    Matcher matcher = pattern.matcher(trimmedText);
+    matcher.matches();
+    String pointString = matcher.group(1);
+    String commentString = matcher.group(2);
+    int points = pointString.isEmpty() ? 4 : Integer.valueOf(pointString);
+    return ExperienceEntry.CreateForPointsAndComment(points, commentString);
   }
 }
