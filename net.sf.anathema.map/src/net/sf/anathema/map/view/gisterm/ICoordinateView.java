e39daa68604b9a042a5e4425580595273b1acf12
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2009-09-02 07:54:17 +0000

    Löschen des gis plugins

--- net.sf.anathema.map/src/net/sf/anathema/map/view/gisterm/ICoordinateView.java
+++
@@ -1,6 +0,0 @@
-package net.sf.anathema.map.view.gisterm;
-
-public interface ICoordinateView extends IView {
-
-  public void setCoordinate(double x, double y);
-}
--- net.sf.anathema.map/src/net/sf/anathema/map/view/gisterm/IView.java
+++
@@ -1,8 +0,0 @@
-package net.sf.anathema.map.view.gisterm;
-
-import javax.swing.JComponent;
-
-public interface IView {
-
-  public JComponent getComponent();
-}
